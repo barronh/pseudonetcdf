956e96745f0a2af88e26d4dc3922390bd545adb8
barronh <barronh@ea03d40b-b492-4883-a61c-0e53214263a1>
2008-12-14 19:03:20 +0000

    Adding documentation
    git-svn-id: https://dawes.sph.unc.edu/uncaqmlsvn/PseudoNetCDF/trunk@287 ea03d40b-b492-4883-a61c-0e53214263a1

--- utils/CAMx/temperature/__init__.py
+++ utils/CAMx/temperature/__init__.py
@@ -1,1 +1,12 @@
+"""
+.. _temperature
+:mod:`temperature` -- Temperature File Interfaces
+=================================================
+
+.. module:: temperature
+   :platform: Unix, Windows
+   :synopsis: Provides :ref:`PseudoNetCDF` memory map and random access read 
+   based file interfaces for CAMx temperature files.
+.. moduleauthor:: Barron Henderson <barronh@unc.edu>
+"""
 __all__=['Memmap','Read','Transforms']
