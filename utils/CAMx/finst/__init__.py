4be33a16e97e79244638d11a7c2431edcaee595c
barronh <barronh@ea03d40b-b492-4883-a61c-0e53214263a1>
2008-12-14 18:59:23 +0000

    Adding documentation and converting tabs to spaces
    git-svn-id: https://dawes.sph.unc.edu/uncaqmlsvn/PseudoNetCDF/trunk@280 ea03d40b-b492-4883-a61c-0e53214263a1

--- utils/CAMx/finst/__init__.py
+++ utils/CAMx/finst/__init__.py
@@ -1,1 +1,12 @@
+"""
+.. _finst
+:mod:`finst` -- Fine UAM-IV File Interfaces
+===========================================
+
+.. module:: finst
+   :platform: Unix, Windows
+   :synopsis: Provides :ref:`PseudoNetCDF` memory map and random access read 
+   based file interfaces for CAMx fine UAM-IV files.
+.. moduleauthor:: Barron Henderson <barronh@unc.edu>
+"""
 __all__=['Memmap']
