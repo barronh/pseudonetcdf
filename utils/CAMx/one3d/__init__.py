ec77bd5334d2903824c6ade50b2657a465efa930
barronh <barronh@ea03d40b-b492-4883-a61c-0e53214263a1>
2008-12-14 19:00:02 +0000

    Adding documentation and converting tabs to spaces
    git-svn-id: https://dawes.sph.unc.edu/uncaqmlsvn/PseudoNetCDF/trunk@281 ea03d40b-b492-4883-a61c-0e53214263a1

--- utils/CAMx/one3d/__init__.py
+++ utils/CAMx/one3d/__init__.py
@@ -1,1 +1,12 @@
+"""
+.. _one3d
+:mod:`one3d` -- one3d File Interfaces
+=====================================
+
+.. module:: one3d
+   :platform: Unix, Windows
+   :synopsis: Provides :ref:`PseudoNetCDF` memory map and random access read 
+   based file interfaces for CAMx one variable 3d files.
+.. moduleauthor:: Barron Henderson <barronh@unc.edu>
+"""
 __all__=['Memmap','Read','Write']
