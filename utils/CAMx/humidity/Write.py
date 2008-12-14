6de59db0f83d66846c3fcea50bceb83a2b4400be
barronh <barronh@ea03d40b-b492-4883-a61c-0e53214263a1>
2008-12-14 19:02:29 +0000

    Adding documentation and converting tabs to spaces
    git-svn-id: https://dawes.sph.unc.edu/uncaqmlsvn/PseudoNetCDF/trunk@285 ea03d40b-b492-4883-a61c-0e53214263a1

--- utils/CAMx/humidity/Write.py
+++ utils/CAMx/humidity/Write.py
@@ -1,2 +1,3 @@
-from pyPA.utils.CAMx.one3d.Write import ncf2one3d
-ncf2hum=ncf2one3d
+from pyPA.utils.CAMx.one3d.Write import ncf2one3d as ncf2hum
+
+__all__ = ['ncf2hum']
--- utils/CAMx/humidity/__init__.py
+++ utils/CAMx/humidity/__init__.py
@@ -1,1 +1,12 @@
+"""
+.. _humidity
+:mod:`humidity` -- Humidity File Interfaces
+===========================================
+
+.. module:: humidity
+   :platform: Unix, Windows
+   :synopsis: Provides :ref:`PseudoNetCDF` memory map and random access read 
+   based file interfaces for CAMx humidity files.
+.. moduleauthor:: Barron Henderson <barronh@unc.edu>
+"""
 __all__=['Memmap','Read','Transforms','Write']
