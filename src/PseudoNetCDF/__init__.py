9ad14a4a5b63c62714a13f21f328bfd7c21f33d1
barronh <barronh@ea03d40b-b492-4883-a61c-0e53214263a1>
2009-03-26 11:21:32 +0000

    racm and cmaq awareness
    Added recursive import ability and __all__ items for racmfiles and cmaqfiles
    
    git-svn-id: https://dawes.sph.unc.edu/uncaqmlsvn/PseudoNetCDF/trunk@351 ea03d40b-b492-4883-a61c-0e53214263a1

--- src/PseudoNetCDF/__init__.py
+++ src/PseudoNetCDF/__init__.py
@@ -4,11 +4,13 @@ NetCDF and manipulating or extending existing NetCDF
 like files
 """
 
-__all__ = ['sci_var', 'MetaNetCDF', 'units', 'ArrayTransforms', 'camxfiles', 'test', 'pncdump']
+__all__ = ['sci_var', 'MetaNetCDF', 'units', 'ArrayTransforms', 'camxfiles', 'test', 'pncdump', 'cmaqfiles', 'racmfiles']
 
 import sci_var
 import MetaNetCDF
 import ArrayTransforms
 import units
 import camxfiles
+import cmaqfiles
+import racmfiles
 import test
