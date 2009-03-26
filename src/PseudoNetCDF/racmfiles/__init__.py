33db7ce5f0d567a34ee97c65fbd09d1bf91d5eeb
barronh <barronh@ea03d40b-b492-4883-a61c-0e53214263a1>
2009-03-26 11:22:02 +0000

    racm2 boxmodel conc reader
    Added a stand alone concentration reader for racm2 box model
    
    git-svn-id: https://dawes.sph.unc.edu/uncaqmlsvn/PseudoNetCDF/trunk@354 ea03d40b-b492-4883-a61c-0e53214263a1

---
+++ src/PseudoNetCDF/racmfiles/__init__.py
@@ -0,0 +1,15 @@
+__doc__ = """
+.. _racmfiles
+:mod:`racmfile` -- RACM File Interfaces
+===================================
+
+.. module:: RACM
+   :platform: Unix, Windows
+   :synopsis: Provides :ref:`PseudoNetCDF`  random access read 
+              based file interfaces for all RACM related files.  See
+              PseudoNetCDF.sci_var.PseudoNetCDFFile for interface details.
+.. moduleauthor:: Barron Henderson <barronh@unc.edu>
+"""
+__all__=['boxmodel']
+
+import boxmodel
---
+++ src/PseudoNetCDF/racmfiles/boxmodel.py
@@ -0,0 +1,37 @@
+from ..sci_var import PseudoNetCDFFile
+from numpy import array, dtype, fromstring, newaxis, vectorize
+from datetime import timedelta, datetime
+
+def box_model_conc(conc_path, start_datetime):
+    lines = file(conc_path,'r').readlines()[3:]
+    
+    spc_names = [spc_name.strip() for spc_name in lines[0][:-1].split('\t')]
+    data_block = ''.join(lines[1:])
+    
+    line_format = dtype(dict(names=spc_names, formats=['f']*len(spc_names)))
+    conc=fromstring(data_block, dtype='f', sep='\t').view(line_format)
+    
+    result = PseudoNetCDFFile()
+    result.SDATE = int(start_datetime.strftime('%Y%j'))
+    result.STIME = int(start_datetime.strftime('%H%M%S'))
+    result.createDimension('TSTEP', conc.shape[0])
+    result.createDimension('VAR', 1)
+    result.createDimension('DATE-TIME', 2)
+    for spc in spc_names:
+        var = result.createVariable(spc,'f',('TSTEP',))
+        var[:] = conc[spc]
+        var.long_name = spc.ljust(16)
+        var.var_desc = spc.ljust(16)
+        var.units = 'ppb'
+
+    tflag = result.createVariable('TFLAG', 'i', ('TSTEP', 'VAR', 'DATE-TIME'))
+    start_date = datetime.strptime(start_datetime.strftime('%Y%j'),'%Y%j')
+    t_tflag = start_date+vectorize(lambda h: timedelta(hours = float(h)))(conc[spc_names[0]].astype('d'))
+    tflag[:, :, 0] = vectorize(lambda d: int(d.strftime('%Y%j')))(t_tflag[:,newaxis])
+    tflag[:, :, 1] = vectorize(lambda d: int(d.strftime('%H%M%S')))(t_tflag[:,newaxis])
+    tflag.units = "<YYYYMMDD, HHMMSS>"
+    tflag.long_name = 'TFLAG'.ljust(16)
+    tflag.var_desc = 'TFLAG'.ljust(16)
+ 
+    return result
+ 
