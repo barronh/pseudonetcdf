d6f47797978a5d95c7be5e1dde40394904199c69
barronh <barronh@ea03d40b-b492-4883-a61c-0e53214263a1>
2009-04-24 15:17:42 +0000

    Setup is now racmfiles aware
    Distributions and installations were missing racmfiles.  setup.py has been modified to include them.
    
    git-svn-id: https://dawes.sph.unc.edu/uncaqmlsvn/PseudoNetCDF/trunk@355 ea03d40b-b492-4883-a61c-0e53214263a1

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(name = 'PseudoNetCDF',
       author_email = 'barronh@gmail.com',
       maintainer = 'Barron Henderson',
       maintainer_email = 'barronh@gmail.com',
-      packages = ['PseudoNetCDF', 'PseudoNetCDF/camxfiles', 'PseudoNetCDF/camxfiles/cloud_rain', 'PseudoNetCDF/camxfiles/finst', 'PseudoNetCDF/camxfiles/height_pressure', 'PseudoNetCDF/camxfiles/humidity', 'PseudoNetCDF/camxfiles/ipr', 'PseudoNetCDF/camxfiles/irr', 'PseudoNetCDF/camxfiles/landuse', 'PseudoNetCDF/camxfiles/one3d', 'PseudoNetCDF/camxfiles/point_source', 'PseudoNetCDF/camxfiles/temperature', 'PseudoNetCDF/camxfiles/uamiv', 'PseudoNetCDF/camxfiles/vertical_diffusivity', 'PseudoNetCDF/camxfiles/wind', 'PseudoNetCDF/cmaqfiles'],
+      packages = ['PseudoNetCDF', 'PseudoNetCDF/camxfiles', 'PseudoNetCDF/camxfiles/cloud_rain', 'PseudoNetCDF/camxfiles/finst', 'PseudoNetCDF/camxfiles/height_pressure', 'PseudoNetCDF/camxfiles/humidity', 'PseudoNetCDF/camxfiles/ipr', 'PseudoNetCDF/camxfiles/irr', 'PseudoNetCDF/camxfiles/landuse', 'PseudoNetCDF/camxfiles/one3d', 'PseudoNetCDF/camxfiles/point_source', 'PseudoNetCDF/camxfiles/temperature', 'PseudoNetCDF/camxfiles/uamiv', 'PseudoNetCDF/camxfiles/vertical_diffusivity', 'PseudoNetCDF/camxfiles/wind', 'PseudoNetCDF/cmaqfiles', 'PseudoNetCDF/racmfiles],
       package_dir = {'': 'src'},
       requires = [pkg, 'numpy (>=0.9)', 'yaml']
       )
