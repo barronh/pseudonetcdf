1cfc94d12e7c80b001e37b379241a04d1ec81249
barronh <barronh@ea03d40b-b492-4883-a61c-0e53214263a1>
2009-06-11 02:00:30 +0000

    Implemented box model mrg
    Updating from edits made at the EPA
    
    git-svn-id: https://dawes.sph.unc.edu/uncaqmlsvn/PseudoNetCDF/trunk@362 ea03d40b-b492-4883-a61c-0e53214263a1

--- src/PseudoNetCDF/racmfiles/boxmodel.py
+++ src/PseudoNetCDF/racmfiles/boxmodel.py
@@ -42,3 +42,45 @@ def box_model_conc(conc_path, start_datetime):
  
     return result
  
+def box_model_mrg(conc_path, irr_path, start_datetime):
+    retval = PseudoNetCDFFile()
+    conc = box_model_conc(conc_path, start_datetime)
+    irr = box_model_irr(irr_path, start_datetime)
+    sorted_rxns = [k for k in irr.variables.keys() if k not in ('TFLAG', 'TIME')]
+    sorted_rxns.sort()
+    sorted_spcs = [k for k in conc.variables.keys() if k not in ('TFLAG', 'TIME')]
+    sorted_spcs.sort()
+
+    nsteps = retval.createDimension('TSTEP', irr.dimensions['TSTEP']-1)
+    nrxns = retval.createDimension('REACTIONS', len(sorted_rxns))
+    nrxns = retval.dimensions['REACTIONS']
+    nsteps = retval.dimensions['TSTEP']
+    retval.createDimension('SPECIES', len(sorted_spcs))
+    retval.createDimension('PROCESS', 3)
+    irr_vals = retval.createVariable('IRR','f',('TSTEP','REACTIONS'))
+    irr_vals.units = 'ppb/h'
+    irr_vals.long_name = 'IRR'.ljust(16)
+    irr_vals.var_desc = 'IRR'.ljust(16)
+    ipr_vals = retval.createVariable('IPR','f',('TSTEP','SPECIES', 'PROCESS'))
+    ipr_vals.units = 'ppb/h'
+    ipr_vals.long_name = 'IPR'.ljust(16)
+    ipr_vals.var_desc = 'IPR'.ljust(16)
+
+    for rxni, rxn_name in enumerate(sorted_rxns):
+        vals = irr.variables[rxn_name][:]
+        vals = vals.repeat(2,0)[1:-1].reshape(nsteps,2).mean(1)
+
+        irr_vals[:,rxni] = vals
+
+    irr_vals[0,:] = nan
+    retval.Reactions = ''.join([k.ljust(16) for k in sorted_rxns])
+
+    for spci, spc_name in enumerate(sorted_spcs):
+        ipr_vals[:,spci,0]=conc.variables[spc_name][:-1]
+        ipr_vals[:,spci,2]=conc.variables[spc_name][1:]
+        ipr_vals[:,spci,1] = ipr_vals[:,spci,2]-ipr_vals[:,spci,0]
+
+    retval.Species = ''.join([k.ljust(16) for k in sorted_spcs])
+    retval.Process = 'INIT'.ljust(16)+'CHEM'.ljust(16)+'FCONC'.ljust(16)
+
+    return retval
