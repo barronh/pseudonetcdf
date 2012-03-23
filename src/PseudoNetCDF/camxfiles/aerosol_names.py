84afd62a89d6e71eaca3907541fcb6a2d3edcd70
bhenders <bhenders@DC2626UBHENDERS.aa.ad.epa.gov>
2012-03-23 16:39:30 -0400

    Adding support for aerosols and aerosol species

---
+++ src/PseudoNetCDF/camxfiles/aerosol_names.py
@@ -0,0 +1,6 @@
+camx_cf_species = ('PSO4', 'PNO3', 'PNH4', 'PH2O', 'SOA1', 'SOA2',\
+'SOA3', 'SOA4', 'SOA5', 'SOA6', 'SOA7', 'SOPA',\
+'SOPB', 'NA', 'PCL', 'POA', 'PEC', 'FPRM', 'FCRS',\
+'CPRM', 'CCRS')
+
+aerosol_names = camx_cf_species
---
+++ src/PseudoNetCDF/camxfiles/units.py
@@ -0,0 +1,12 @@
+from PseudoNetCDF.camxfiles.aerosol_names import aerosol_names
+
+_camx_units = {'EMISSIONS ': {True: 'g/time', False: 'mol/time'},
+               'AVERAGE   ': {True: 'micrograms/m**3', False: 'ppm'},
+               'INSTANT   ': {True: 'micrograms/m**3', False: 'micromoles/m**3'},
+               'IPR': {True: 'micrograms/m**3', False: 'micromoles/m**3'},
+               'AIRQUALITY': {True: 'micrograms/m**3', False: 'micromoles/m**3'}}
+
+def get_uamiv_units(filename, key):
+    from PseudoNetCDF.camxfiles.aerosol_names import aerosol_names
+    return _camx_units[filename][key in aerosol_names]
+
