46332d2e02d015b019cd5116d9b182e4728b5a52
Barron Henderson <barronh@gmail.com>
2013-07-15 20:48:27 -0400

    Switching default output to be NETCDF4_CLASSIC to that it can be easily converted to nc3

--- src/PseudoNetCDF/pncgen.py
+++ src/PseudoNetCDF/pncgen.py
@@ -2,7 +2,7 @@ from types import MethodType
 from sci_var import Pseudo2NetCDF
 from netcdf import NetCDFFile
 
-def pncgen(f,outpath, inmode = 'r', outmode = 'w', format = 'NETCDF4'):
+def pncgen(f,outpath, inmode = 'r', outmode = 'w', format = 'NETCDF4_CLASSIC'):
     p2n = Pseudo2NetCDF()
     return p2n.convert(f, outpath, inmode = inmode, outmode = outmode, format = format)
     
