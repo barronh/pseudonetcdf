785f8145e3f5752d2e1ac290caf0a36a1e4664d1
Barron Henderson <barronh@gmail.com>
2013-08-05 16:12:45 -0400

    Adding ipr_read and irr_read options

--- src/PseudoNetCDF/pncdump.py
+++ src/PseudoNetCDF/pncdump.py
@@ -227,6 +227,7 @@ def pncdump(f, name = 'unknown', header = False, variables = [], line_length = 8
 if __name__ == '__main__':
     from optparse import OptionParser
     from camxfiles.Memmaps import *
+    from camxfiles.Readers import irr as irr_read, ipr as ipr_read
     from icarttfiles.ffi1001 import ffi1001
     from geoschemfiles import *
     try:
