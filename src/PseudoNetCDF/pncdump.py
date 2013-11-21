dd78c1310f4e44aabde2234c72e6380d5484263b
Barron Henderson <barronh@gmail.com>
2013-11-20 20:46:15 -0500

    Allowing pncdump to dump files with groups.

--- src/PseudoNetCDF/pncdump.py
+++ src/PseudoNetCDF/pncdump.py
@@ -18,7 +18,7 @@ import textwrap
 import sys
 import operator
 
-def pncdump(f, name = 'unknown', header = False, variables = [], line_length = 80, full_indices = None, float_precision = 8, double_precision = 16):
+def pncdump(f, name = 'unknown', header = False, variables = [], line_length = 80, full_indices = None, float_precision = 8, double_precision = 16, isgroup = False):
     """
     pncdump is designed to implement basic functionality
     of the NetCDF ncdump binary.
@@ -45,21 +45,26 @@ def pncdump(f, name = 'unknown', header = False, variables = [], line_length = 8
     # initialize indentation as 8 characters
     # based on ncdump
     indent = 8*" "
+    if isgroup:
+        startindent = 4*" "
+    else:
+        startindent = 4*""
         
     # First line of CDL
-    sys.stdout.write("netcdf %s {\n" % (name,))
+    if not isgroup: sys.stdout.write("netcdf %s {\n" % (name,))
     
     ###########################
     # CDL Section 1: dimensions
     ###########################
-    sys.stdout.write("dimensions:\n")
+    sys.stdout.write(startindent + "dimensions:\n")
     for dim_name, dim in f.dimensions.iteritems():
-        sys.stdout.write(1*indent+("%s = %s ;\n" % (dim_name,len(dim))))
+        sys.stdout.write(startindent + 1*indent+("%s = %s ;\n" % (dim_name,len(dim))))
     
     ###################################
     # CDL Section 2: variables metadata
     ###################################
-    sys.stdout.write("\nvariables:\n")
+    if len(f.variables.keys()) > 0:
+        sys.stdout.write("\n" + startindent + "variables:\n")
     for var_name, var in f.variables.iteritems():
         var_type = dict(float32='float', \
                         float64='double', \
@@ -67,10 +72,10 @@ def pncdump(f, name = 'unknown', header = False, variables = [], line_length = 8
                         int64='long', \
                         bool='bool', \
                         string8='char')[var.dtype.name]
-        sys.stdout.write(1*indent+("%s %s%s;\n" % (var_type, var_name,str(var.dimensions).replace('u\'', '').replace('\'','').replace(',)',')'))))
+        sys.stdout.write(startindent + 1*indent+("%s %s%s;\n" % (var_type, var_name,str(var.dimensions).replace('u\'', '').replace('\'','').replace(',)',')'))))
         for prop_name in var.ncattrs():
             prop = getattr(var, prop_name)
-            sys.stdout.write(2*indent+("%s:%s = %s ;\n" % (var_name,prop_name,repr(prop).replace("'", '"'))))
+            sys.stdout.write(startindent + 2*indent+("%s:%s = %s ;\n" % (var_name,prop_name,repr(prop).replace("'", '"'))))
     
     ################################
     # CDL Section 3: global metadata
@@ -78,8 +83,12 @@ def pncdump(f, name = 'unknown', header = False, variables = [], line_length = 8
     sys.stdout.write("\n\n// global properties:\n")
     for prop_name in f.ncattrs():
         prop = getattr(f, prop_name)
-        sys.stdout.write(2*indent+(":%s = %s ;\n" % (prop_name, repr(prop).replace("'",'"'))))
+        sys.stdout.write(startindent + 2*indent+(":%s = %s ;\n" % (prop_name, repr(prop).replace("'",'"'))))
 
+    if hasattr(f, 'groups'):
+        for group_name, group in f.groups.iteritems():
+            sys.stdout.write(startindent + 'group %s:\n' % group_name)
+            pncdump(group, name = name, header = header, variables = variables, line_length = line_length, full_indices = full_indices, float_precision = float_precision, double_precision = double_precision, isgroup = True)
     if not header:
         # Error trapping prevents the user from getting an error
         # when they cancel a dump or when they break a redirected
@@ -88,7 +97,7 @@ def pncdump(f, name = 'unknown', header = False, variables = [], line_length = 8
             #####################
             # CDL Section 4: data
             #####################
-            sys.stdout.write("\n\ndata:\n")
+            sys.stdout.write("\n\n" + startindent + "data:\n")
             
             # data indentation is only 1 space
             indent = " "
@@ -103,13 +112,13 @@ def pncdump(f, name = 'unknown', header = False, variables = [], line_length = 8
             # currently assumes 3-D data
             for var_name in display_variables:
                 var = f.variables[var_name]
-                sys.stdout.write(1*indent+("%s =\n" % var_name))
+                sys.stdout.write(startindent + 1*indent+("%s =\n" % var_name))
                 if full_indices is not None:
                     id_display = {'f': lambda idx: str(tuple([idx[i]+1 for i in range(len(idx)-1,-1,-1)])), \
                                   'c': lambda idx: str(idx)}[full_indices]
                                   
                     for i, val in ndenumerate(var):
-                        fmt = 2*indent+formats[var.dtype.name]
+                        fmt = startindent + 2*indent+formats[var.dtype.name]
                         array_str = fmt % val
                         if i == tuple(map(lambda x_: x_ - 1, var.shape)):
                             array_str += ";"
@@ -144,7 +153,7 @@ def pncdump(f, name = 'unknown', header = False, variables = [], line_length = 8
                             array_str += ','
                             
                         try:
-                            sys.stdout.write(textwrap.fill(array_str, line_length, initial_indent = '  ', subsequent_indent = '    '))
+                            sys.stdout.write(textwrap.fill(array_str, line_length, initial_indent = startindent + '  ', subsequent_indent = startindent + '    '))
                             sys.stdout.write('\n')
                         except IOError, e:
                             warn(repr(e) + "; Typically from CTRL+C or exiting less")
