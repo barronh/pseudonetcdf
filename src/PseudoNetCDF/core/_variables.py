b169321e9f4abbd7dedd6a2eeaa007c7a03508b5
Barron Henderson <barronh@gmail.com>
2014-03-07 13:07:56 -0500

    Adding priority of array.

--- src/PseudoNetCDF/core/_variables.py
+++ src/PseudoNetCDF/core/_variables.py
@@ -8,6 +8,7 @@ class PseudoNetCDFVariable(np.ndarray):
     but unlike that type, provides a contructor for variables that could be used
     without adding it to the parent file
     """
+    __array_priority__ = 100000000.
     def __setattr__(self, k, v):
         """
         Set attributes (aka properties) and identify user-defined attributes.
