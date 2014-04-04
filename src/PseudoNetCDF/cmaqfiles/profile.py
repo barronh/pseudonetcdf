d730ecfa0067cbcd89cca6df924816dc3e49c128
Barron Henderson <barronh@gmail.com>
2014-04-03 21:56:39 -0400

    Adding icon and bcon versions
    Profiles can have 4 sides or a single vertical profile.

--- src/PseudoNetCDF/cmaqfiles/profile.py
+++ src/PseudoNetCDF/cmaqfiles/profile.py
@@ -1,11 +1,45 @@
-__all__ = ['profile']
+__all__ = ['bcon_profile', 'icon_profile']
 from PseudoNetCDF import PseudoNetCDFFile, PseudoNetCDFVariables
 from matplotlib.mlab import csv2rec
 from StringIO import StringIO
 from datetime import datetime
 import numpy as np
+class icon_profile(PseudoNetCDFFile):
+    def __init__(self, path):
+        lines = file(path).read().split('\n')
+        header = lines[3].split()
+        nlay, nspc = map(int, header[:2])
+        sigmas = map(float, header[2:])
+        nsigmas = len(sigmas)
+        try:
+            dateo = datetime.strptime(lines[4].strip(), '%Y-%m-%d')
+        except:
+            date, time = map(int, lines[4].split())
+        starts =  [5]
+        ends = [s + nspc for s in starts]
+        keys = ['all']
+        fieldnames = ('name',) + tuple(['s%f' % i for i in sigmas])
+        data = dict([(k, csv2rec(StringIO('\n'.join(lines[s:e])), delimiter = ' ', names = fieldnames, converterd = dict(names = lambda x: str(x).strip()))) for k, s, e in zip(keys, starts, ends)])
+        profile_spcs = np.char.strip(data[keys[0]]['name'])
+        data_type = data[keys[0]].dtype
+        data_shape =  data[keys[0]].shape
+        self.createDimension('sigma', nsigmas)
+        self.createDimension('sigma-mid', nsigmas - 1)
+        self.createDimension('senw', 4)
+        self.createVariable('sigma', 'f', ('sigma',), values = np.array(sigmas), units = 'sigma')
+        self.createVariable('sigma-mid', 'f', ('sigma-mid',), values = np.array(sigmas).repeat(2, 0)[1:-1].reshape(-1, 2).mean(1), units = 'sigma')
+        ks = keys[1:]
+        for k in ks:
+            try:
+                assert((np.char.strip(data[k]['name']) == profile_spcs).all())
+                assert(data[k].dtype == data_type)
+                assert(data[k].dtype == data_type)
+            except AssertionError:
+                raise IOError('File is corrupt or inconsistent')
+        for a in data['all']:
+            self.createVariable(a[0].strip(), 'f', ('sigma-mid', 'senw'), units = "None", values = np.array(map(lambda x: tuple(x)[1:], [a])).T, long_name = a[0].ljust(16), var_desc = a[0].ljust(16))
 
-class profile(PseudoNetCDFFile):
+class bcon_profile(PseudoNetCDFFile):
     def __init__(self, path):
         lines = file(path).read().split('\n')
         header = lines[3].split()
@@ -24,8 +58,8 @@ class profile(PseudoNetCDFFile):
         profile_spcs = np.char.strip(data[keys[0]]['name'])
         data_type = data[keys[0]].dtype
         data_shape =  data[keys[0]].shape
-        self.createDimension('sigma', nsigmas + 1)
-        self.createDimension('sigma-mid', nsigmas)
+        self.createDimension('sigma', nsigmas)
+        self.createDimension('sigma-mid', nsigmas - 1)
         self.createDimension('senw', 4)
         self.createVariable('sigma', 'f', ('sigma',), values = np.array(sigmas), units = 'sigma')
         self.createVariable('sigma-mid', 'f', ('sigma-mid',), values = np.array(sigmas).repeat(2, 0)[1:-1].reshape(-1, 2).mean(1), units = 'sigma')
