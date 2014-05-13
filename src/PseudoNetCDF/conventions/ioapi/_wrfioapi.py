dc5a01ee5cda15d3a630dd0cb1d2c7d95097a902
Barron Henderson <barronh@gmail.com>
2014-05-12 21:51:25 -0400

    Fixing WRF-IOAPI

--- src/PseudoNetCDF/conventions/ioapi/_wrfioapi.py
+++ src/PseudoNetCDF/conventions/ioapi/_wrfioapi.py
@@ -6,6 +6,7 @@ def add_cf_from_wrfioapi(ifile):
         outvar = ifile.createVariable(outvark, invar.dtype.char, invar.dimensions[1:])
         for pk in invar.ncattrs():
             setattr(outvar, pk, getattr(invar, pk))
+        outvar[:] = invar[0]
     for k in ifile.variables.keys():
         var = ifile.variables[k]
         var.coordinates = ' '.join([_coorddict.get(dk, dk) for dk in var.dimensions])
