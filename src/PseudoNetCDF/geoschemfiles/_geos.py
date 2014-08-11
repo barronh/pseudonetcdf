b5ab2a66b92af7d3139e55a72b1ec223b073607f
Barron Henderson <barronh@gmail.com>
2014-08-11 15:51:21 -0400

    Adding dynamic layer lumping

--- src/PseudoNetCDF/geoschemfiles/_geos.py
+++ src/PseudoNetCDF/geoschemfiles/_geos.py
@@ -66,6 +66,12 @@ PS = 'hPa',
 SLP = 'hPa',
 TO3 = 'DU',
 TTO3 = 'DU')
+
+def lump(data, ap, levels):
+    ldata = data[:, levels]
+    lweights = -np.diff(ap)[levels][None, :, None, None]
+    return ldata * lweights / lweights.sum(1)[:, None]
+    
 class geos(PseudoNetCDFFile):
     def __init__(self, path, mode = 'r', reduced = True):
         from _vertcoord import geos_hyai, geos_hybi, geos_etam, geos_etai
@@ -87,13 +93,16 @@ class geos(PseudoNetCDFFile):
             if first:
                 pass
             else:
-                if elem == (nrow * ncol + 2):
+                if elem == 1:
+                   pass # shape unknown
+                elif elem == (nrow * ncol + 2):
                    shape = (nrow, ncol)
-                elif elem == (nrow * ncol * nlay + 2):
-                   shape = (nlay, nrow, ncol)
-                elif elem == (nrow * ncol * nlay_stag + 2):
-                   shape = (nlay_stag, nrow, ncol)
-                
+                elif elem == (nrow * ncol * nlay_in + 2):
+                   shape = (nlay_in, nrow, ncol)
+                elif elem == (nrow * ncol * nlay_in_stag + 2):
+                   shape = (nlay_in_stag, nrow, ncol)
+                else:
+                   raise IOError('Expected %d, %d, or %d elements, got %d' % (nrow * ncol + 2, nrow * ncol * nlay + 2, nrow * ncol * nlay_stag + 2, elem))
             if dtype == '>S8':
                 data = fromfile(infile, dtype = dtype, count = elem)
             else:
@@ -111,26 +120,27 @@ class geos(PseudoNetCDFFile):
                     first = False
                     if name[:2] == 'G5':
                         etak = 'GEOS-5-' + ('REDUCED' if reduced else 'NATIVE')
-                        # Ap [hPa]
-                        self.Ap = geos_hyai[etak]
-                        # Bp [unitless]
-                        self.Bp = geos_hybi[etak]
-                        eta_m = geos_etam[etak]
-                        eta_i = geos_etai[etak]
-                        nlay = self.Ap.size - 1
-                        nlay_stag = self.Ap.size
+                        etafk = 'GEOS-5-' + 'NATIVE'
                     elif name[:2] == 'G4':
                         etak = 'GEOS-4-' + ('REDUCED' if reduced else 'NATIVE')
-                        # Ap [hPa]
-                        self.Ap = geos_hyai[etak]
-                        # Bp [unitless]
-                        self.Bp = geos_hybi[etak]
-                        eta_m = geos_etam[etak]
-                        eta_i = geos_etai[etak]
-                        nlay = self.Ap.size - 1
-                        nlay_stag = self.Ap.size
+                        etafk = 'GEOS-4-' + 'NATIVE'
                     else:
                         raise ValueError('Unknown type %s' % name)
+                    self.gtype = etak
+                    # Ap [hPa]
+                    self.Ap = geos_hyai[etak]
+                    # Bp [unitless]
+                    self.Bp = geos_hybi[etak]
+                    # Ap full [hPa]
+                    self.Ap_NATIVE = geos_hyai[etafk]
+                    # Bp full [unitless]
+                    self.Bp_NATIVE = geos_hybi[etafk]
+                    eta_m = geos_etam[etak]
+                    eta_i = geos_etai[etak]
+                    nlay = self.Ap.size - 1
+                    nlay_stag = self.Ap.size
+                    nlay_in = self.Ap_NATIVE.size - 1
+                    nlay_in_stag = self.Ap_NATIVE.size
 
                     if name[3:5] == '22':
                         longitude_bounds = np.arange(-181.25, 180, 2.5).repeat(2, 0)[1:-1].reshape(-1, 2)
@@ -176,7 +186,42 @@ class geos(PseudoNetCDFFile):
                 dims = ('time', 'layer', 'latitude', 'longitude')
             else:
                 dims = ('time', 'layer_stag', 'latitude', 'longitude')
-            unit = _geos_units.get(key, '')    
+            unit = _geos_units.get(key, '')
+            if dims != ('time', 'latitude', 'longitude'):
+                thisdatain = thisdata
+                thisdata = np.zeros(map(lambda k: len(self.dimensions[k]), dims), dtype = thisdata.dtype)
+                if reduced:
+                    if self.gtype == 'GEOS-4-REDUCED':
+                        #!--------------------------------------------------------------
+                        #! GEOS-4: Lump 55 levels into 30 levels, starting above L=20
+                        #! Lump levels in groups of 2, then 4. (cf. Mat Evans)
+                        #!--------------------------------------------------------------
+                        #
+                        #! Lump 2 levels together at a time
+                        lump_groups = [(0,), (1,), (2,), (3,), (4,), (5,), (6,), (7,), (8,), (9,), (10,), (11,), (12,), (13,), (14,), (15,), (16,), (17,), (18,)] + \
+                                 [(19, 20), (21, 22), (23, 24), (25, 26)] + \
+                                 [(27, 28, 29, 30), (31, 32, 33, 34), (35, 36, 37, 38), (39, 40, 41, 42), (43, 44, 45, 46), (47, 48, 49, 50), (51, 52, 53, 54)]
+
+                    elif self.gtype == 'GEOS-5-REDUCED':
+                        #!--------------------------------------------------------------
+                        #! GEOS-5/MERRA: Lump 72 levels into 47 levels, starting above 
+                        #! L=36.  Lump levels in groups of 2, then 4. (cf. Bob Yantosca)
+                        #!--------------------------------------------------------------
+                        #
+                        #! Lump 2 levels together at a time
+                        lump_groups = [(0,), (1,), (2,), (3,), (4,), (5,), (6,), (7,), (8,), (9,), (10,), (11,), (12,), (13,), (14,), (15,), (16,), (17,), (18,), (19,), (20,), (21,), (22,), (23,), (24,), (25,), (26,), (27,), (28,), (29,), (30,), (31,), (32,), (33,), (34,), (35,)] + \
+                                [(36, 37), (38, 39), (40, 41), (42, 43)] + \
+                                [(44, 45, 46, 47), (48, 49, 50, 51), (52, 53, 54, 55), (56, 57, 58, 59), (60, 61, 62, 63), (64, 65, 66, 67), (68, 69, 70, 71)]
+                    else:
+                        raise ValueError('Cannot reduce %' % self.gtype)
+                    assert(len(lump_groups) == nlay)
+                    for li, lump_group in enumerate(lump_groups):
+                        if len(lump_group) == 1 or dims[1] == 'layer_stag':
+                            thisdata[:, li] = thisdatain[:, lump_group[0]]
+                        elif dims[1] == 'layer':
+                            thisdata[:, li] = lump(thisdatain, self.Ap, lump_group)
+                        else:
+                            raise ValueError('huh?')
             return PseudoNetCDFVariable(self, key, 'f', dims, values = thisdata, units = unit, long_name = key.ljust(16))
         self.variables = PseudoNetCDFVariables(keys = names[1:], func = getem)
         dates = data[0]['data'][names[1]]['f4']
