db0b37fee3b3cd220593fce648009a830e101ec1
Barron Henderson <barronh@gmail.com>
2015-05-07 19:07:14 -0400

    Added CAMx VBS nomenclature

--- src/PseudoNetCDF/camxfiles/aerosol_names.py
+++ src/PseudoNetCDF/camxfiles/aerosol_names.py
@@ -1,6 +1,6 @@
 camx_cf_species = ('PSO4', 'PNO3', 'PNH4', 'PH2O', 'SOA1', 'SOA2',\
 'SOA3', 'SOA4', 'SOA5', 'SOA6', 'SOA7', 'SOPA',\
 'SOPB', 'NA', 'PCL', 'POA', 'PEC', 'FPRM', 'FCRS',\
-'CPRM', 'CCRS')
+'CPRM', 'CCRS', 'PAP0', 'PAP1', 'PAP2', 'PAP3', 'PAP4', 'PAS0', 'PAS1', 'PAS2', 'PAS3', 'PAS4', 'PBS0', 'PBS1', 'PBS2', 'PBS3', 'PBS4', 'PFP0', 'PFP1', 'PFP2', 'PFP3', 'PFP4')
 
 aerosol_names = camx_cf_species
