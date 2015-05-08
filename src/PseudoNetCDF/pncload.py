560f0ae1d8780bb6723f216bb22fd9d47ebd8cfb
Barron Henderson <barronh@gmail.com>
2015-05-08 13:16:07 -0400

    Adding documentation and ease of use

--- src/PseudoNetCDF/pncload.py
+++ src/PseudoNetCDF/pncload.py
@@ -18,12 +18,24 @@ import atexit
 import os
 
 class PNCConsole(code.InteractiveConsole):
+    """
+    The PNCConsole is designed to create a fast Integrated
+    Development Environment for scientific analysis.
+    """
     def __init__(self, locals = None, filename = '<console>',
                 histfile = os.path.expanduser("~/.pncload-history")):
-        code.InteractiveConsole.__init__(self)
+        """
+        locals - dictionary of local variables
+        filename - filename for error printouts
+        histfile - path for history
+        """
+        code.InteractiveConsole.__init__(self, locals = locals, filename = filename)
         self.init_history(histfile)
     
     def init_history(self, histfile):
+        """
+        Prepare history for use from histfile (typically last session)
+        """
         readline.parse_and_bind("tab: complete")
         if hasattr(readline, "read_history_file"):
             try:
@@ -34,28 +46,44 @@ class PNCConsole(code.InteractiveConsole):
             atexit.register(self.save_history, histfile)
             
     def save_history(self, histfile):
+        """
+        Write history from session to disk
+        """
         readline.write_history_file(histfile)
 
 _torem = ['(', ')', '[', ']', '{', '}', '@', ',', ':', '.', '`', '=', ';', '+=', '-=', '*=', '/=', '//=', '%=', '&=', '|=', '^=', '>>=', '<<=', '**=', '+', '-', '*', '**', '/', '//', '%', '<<', '>>', '&', '|', '^', '~', '<', '>', '<=', '>=', '==', '!=', '<>']
 def _clean(p):
+    """
+    Remove operator symbols from file names
+    """
     for d in _torem:
-        p = p.replace(d, '')
+        p = os.path.basename(p).replace(d, '')
     return p
 
 def createconsole(ifiles, options):
+    """
+    Use standard pncparse ifiles and options to create
+    a working environment.
+    
+    1) Access files by a short name or indexed name (ifile%d)
+    2) Access variables by name and file index (var_%d)
+    3) Has access to pylab and pncview functions
+    """
     console = PNCConsole()
     exec("from pylab import *", None, console.locals)
     exec("from PseudoNetCDF.pncview import *; interactive(True)", None, console.locals)
-    ipaths = [_clean(ipath) for ipath in options.ifile]
+    ipaths = [_clean(ipath) for ipath in options.ipath]
     spaths = [ipath[:6] for ipath in ipaths]
     spathsoc = dict([(k, 0) for k in spaths])
     spathso = []
-    for spath in spaths:
+    npathso = []
+    for filei, spath in enumerate(spaths):
         spathso.append(spath + '_' + str(spathsoc[spath]))
         spathsoc[spath] += 1
-    
-    for filei, (ipath, spath, ifile) in enumerate(zip(ipaths, spathso, ifiles)):
-        npath = 'ifile%d' % filei
+        npathso.append('ifile%d' % filei)
+    for rpath, npath, spath in zip(options.ipath, npathso, spathso):
+        print spath + ' = ' + npath + ' = ' + rpath
+    for filei, (ipath, npath, spath, ifile) in enumerate(zip(ipaths, npathso, spathso, ifiles)):
         console.locals[npath] = ifile
         exec(ipath + ' = ' + npath, None, console.locals)
         exec(spath + ' = ' + npath, None, console.locals)
