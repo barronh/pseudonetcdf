52c9ab093492c0798d005e320a027b62705280e5
Barron Henderson <barronh@gmail.com>
2015-06-11 10:30:54 -0400

    Adding plotting scripts.
    These can be used directly or copied and updated for specific use.

---
+++ scripts/pncboundaries.py
@@ -0,0 +1,222 @@
+#!/usr/bin/env python
+import sys
+import numpy as np
+from warnings import warn
+from collections import defaultdict
+from netCDF4 import MFDataset, Dataset
+from datetime import datetime, timedelta
+from PseudoNetCDF.coordutil import getmap
+
+
+def plot(ifiles, args):
+    import pylab as pl
+    from pylab import figure, NullFormatter, close, rcParams
+    from PseudoNetCDF.coordutil import getsigmamid, getpresmid
+    rcParams['text.usetex'] = False
+    from matplotlib.colors import LinearSegmentedColormap, BoundaryNorm, LogNorm
+    map = not args.nomap
+    scale = args.scale
+    minmax = eval(args.minmax)
+    minmaxq = eval(args.minmaxq)
+    sigma = args.sigma
+    maskzeros = args.maskzeros
+    try:
+        f, = ifiles
+    except:
+        raise ValueError('curtain plot expects one file when done. Try stack time --stack=time to concatenate')
+    
+    if sigma:
+        vertcrd = getsigmamid(f)
+    else:
+        vertcrd = getpresmid(f, pref = 101325., ptop = getattr(f, 'VGTOP', 10000))
+        if vertcrd.max() > 2000:  vertcrd /= 100.
+
+
+    reversevert = not (np.diff(vertcrd) > 0).all()
+    for var_name in args.variables:
+        temp = defaultdict(lambda: 1)
+        try:
+            eval(var_name, None, temp)
+            var = eval(var_name, None, f.variables)[:]
+        except:
+            temp[var_name]
+            var = f.variables[var_name][:]
+        if args.itertime:
+            vars = [('time%02d' % vi, v) for vi, v in enumerate(var)]
+        else:
+            vars = [('', var[0])]
+        for lstr, var in vars:
+            bmap = None
+            if maskzeros: var = np.ma.masked_values(var, 0)
+            vmin, vmax = np.percentile(np.ma.compressed(var).ravel(), list(minmaxq))
+            if minmax[0] is not None:
+                vmin = minmax[0]
+            if minmax[1] is not None:
+                vmax = minmax[1]
+            if scale == 'log':
+                bins = np.logspace(np.log10(vmin), np.log10(vmax), 11)
+            elif scale == 'linear':
+                bins = np.linspace(vmin, vmax, 11)
+            elif scale == 'deciles':
+                bins = np.percentile(np.ma.compressed(np.ma.masked_greater(np.ma.masked_less(var, vmin), vmax)).ravel(), [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100])
+                bins[0] = vmin; bins[-1] = vmax
+            norm = BoundaryNorm(bins, ncolors = 256)
+            if map:
+                fig = pl.figure(figsize = (8, 8))
+                fig.subplots_adjust(hspace = .3, wspace = .3)
+                axmap = fig.add_subplot(3,3,5)
+                try:
+                    cmaqmap = getmap(f)
+                    cmaqmap.drawcoastlines(ax = axmap)
+                    cmaqmap.drawcountries(ax = axmap)
+                    cmaqmap.drawstates(ax = axmap)
+                    cmaqmap.drawparallels(np.arange(-90, 100, 10), labels = [True, True, False, False], ax = axmap)
+                    cmaqmap.drawmeridians(np.arange(-180, 190, 20), labels = [False, False, True, True], ax = axmap)
+                except Exception, e:
+                    warn('An error occurred and no map will be shown:\n%s' % str(e))
+                axn = fig.add_subplot(3,3,2, sharex = axmap)
+                axw = fig.add_subplot(3,3,4, sharey = axmap)
+                axe = fig.add_subplot(3,3,6, sharey = axmap)
+                axs = fig.add_subplot(3,3,8, sharex = axmap)
+                cax = fig.add_axes([.8, .7, .05, .25])
+                for ax in [axmap, axe]:
+                    ax.yaxis.set_major_formatter(NullFormatter())
+                for ax in [axmap, axn]:
+                    ax.xaxis.set_major_formatter(NullFormatter())
+                for ax in [axn, axs]:
+                    if sigma:
+                        ax.set_ylabel('sigma')
+                    else:
+                        ax.set_ylabel('pressure')
+                for ax in [axe, axw]:
+                    if sigma:
+                        ax.set_xlabel('sigma')
+                    else:
+                        ax.set_xlabel('pressure')
+                xyfactor = 1
+            else:
+                fig = pl.figure(figsize = (16, 4))
+                fig.subplots_adjust(bottom=0.15)
+                axw = fig.add_subplot(1,4,1)
+                axn = fig.add_subplot(1,4,2)
+                axe = fig.add_subplot(1,4,3)
+                axs = fig.add_subplot(1,4,4)
+                cax = fig.add_axes([.91, .1, .025, .8])
+                if sigma:
+                    axw.set_ylabel('sigma')
+                else:
+                    axw.set_ylabel('pressure')
+            
+                xyfactor = 1e-3 # m -> km
+                     
+            x = f.NCOLS + 1
+            y = f.NROWS + 1
+            start_south = 0
+            end_south = start_south + x
+            start_east = end_south
+            end_east = start_east + y
+            start_north = end_east
+            end_north = start_north + x
+            start_west = end_north
+            end_west = start_west + y
+            X, Y = np.meshgrid(np.arange(x) * f.XCELL * xyfactor, vertcrd)
+            patchess = axs.pcolor(X, Y, var[:, start_south:end_south], cmap = bmap, vmin = vmin, vmax = vmax, norm = norm)
+            if not map:
+                if reversevert: axs.set_ylim(*axs.get_ylim()[::-1])
+                axs.set_title('South')
+                axs.set_xlabel('E to W km')
+                axs.set_xlim(*axs.get_xlim()[::-1])
+                
+            X, Y = np.meshgrid(np.arange(-1, x - 1) * f.XCELL * xyfactor, vertcrd)
+            patchesn = axn.pcolor(X, Y, var[:, start_north:end_north], cmap = bmap, vmin = vmin, vmax = vmax, norm = norm)
+            if reversevert: axn.set_ylim(*axn.get_ylim()[::-1])
+            if not map:
+                axn.set_title('North')
+                axn.set_xlabel('W to E km')
+
+            if map:
+                X, Y = np.meshgrid(vertcrd, np.arange(y) * f.YCELL)
+                patchese = axe.pcolor(X, Y, var[:, start_east:end_east].swapaxes(0,1), cmap = bmap, vmin = vmin, vmax = vmax, norm = norm)
+                if reversevert: axe.set_xlim(*axe.get_xlim()[::-1])
+            else:
+                X, Y = np.meshgrid(np.arange(y) * f.YCELL * xyfactor, vertcrd)
+                patchese = axe.pcolor(X, Y, var[:, start_east:end_east], cmap = bmap, vmin = vmin, vmax = vmax, norm = norm)
+                if reversevert: axe.set_ylim(*axe.get_ylim()[::-1])
+                axe.set_title('East')
+                axe.set_xlabel('N to S km')
+                axe.set_xlim(*axe.get_xlim()[::-1])
+            if map:
+                X, Y = np.meshgrid(vertcrd, np.arange(-1, y - 1) * f.YCELL)
+                patchesw = axw.pcolor(X, Y, var[:, start_west:end_west].swapaxes(0,1), cmap = bmap, vmin = vmin, vmax = vmax, norm = norm)
+            else:
+                X, Y = np.meshgrid(np.arange(-1, y - 1) * f.YCELL * xyfactor, vertcrd)
+                patchesw = axw.pcolor(X, Y, var[:, start_west:end_west], cmap = bmap, vmin = vmin, vmax = vmax, norm = norm)
+                if reversevert: axw.set_ylim(*axw.get_ylim()[::-1])
+                axw.set_title('West')
+                axw.set_xlabel('S to N km')
+            if map:
+                for ax in [axe, axw]:
+                    ax.axis('tight', axis = 'x')
+                    pl.setp( ax.xaxis.get_majorticklabels(), rotation=90 )
+                for ax in [axs, axn]:
+                    ax.axis('tight', axis = 'y')
+            else:
+                for ax in [axe, axn, axw, axs] + ([axmap] if map else []):
+                    ax.axis('tight')
+            
+            if 'TFLAG' in f.variables.keys():
+                SDATE = f.variables['TFLAG'][:][0, 0, 0]
+                EDATE = f.variables['TFLAG'][:][-1, 0, 0]
+                STIME = f.variables['TFLAG'][:][0, 0, 1]
+                ETIME = f.variables['TFLAG'][:][-1, 0, 1]
+                if SDATE == 0:
+                    SDATE = 1900001
+                    EDATE = 1900001
+                sdate = datetime.strptime('%07d %06d' % (SDATE, STIME), '%Y%j %H%M%S')
+                edate = datetime.strptime('%07d %06d' % (EDATE, ETIME), '%Y%j %H%M%S')
+            elif 'tau0' in f.variables.keys():
+                sdate = datetime(1985, 1, 1, 0) + timedelta(hours = f.variables['tau0'][0])
+                edate = datetime(1985, 1, 1, 0) + timedelta(hours = f.variables['tau1'][-1])
+            else:
+                sdate = datetime(1900, 1, 1, 0)
+                edate = datetime(1900, 1, 1, 0)
+            try:
+                title = '%s %s to %s' % (var_name, sdate.strftime('%Y-%m-%d'), edate.strftime('%Y-%m-%d'))
+            except:
+                title = var_name
+            fig.suptitle(title.replace('O3', 'Ozone at Regional Boundaries'))
+            fig.colorbar(patchesw, cax = cax, boundaries = bins)
+            cax.set_xlabel('ppm')
+            fig.savefig('%s_%s%s_%s.%s' % (args.outpath, var_name, lstr, 'boundary', args.figformat))
+            pl.close(fig)
+    
+if __name__ == '__main__':
+    from PseudoNetCDF.pncparse import getparser, pncparse
+    
+    parser = getparser(has_ofile = True, plot_options = True, interactive = False)
+    parser.add_argument("--itertime", dest = "itertime", action = "store_true", default = False,
+                        help = "Create plots for each time.")
+
+    parser.add_argument("-n", "--no-map", dest = "nomap", action = "store_true", default = False,
+                        help = "Try to plot with map")
+
+    parser.add_argument("--sigma", dest = "sigma", action = "store_true", default = False,
+                        help = "Plot data on sigma coordinate.")
+
+    parser.add_argument("--scale", dest = "scale", type = str, default = 'deciles',
+                        help = "Defaults to deciles (i.e., 10 equal probability bins), but linear and log are also options.")
+
+    parser.add_argument("--mask-zeros", dest = "maskzeros", action = "store_true", default = False,
+                        help = "Defaults False.")
+
+    parser.add_argument("--minmax", dest = "minmax", type = str, default = "None,None",
+                        help = "Defaults None, None.")
+
+    parser.add_argument("--minmaxq", dest = "minmaxq", type = str, default = '0,100',
+                        help = "Defaults 0,100.")
+
+    ifiles, args = pncparse(has_ofile = True, parser = parser)
+    if args.variables is None:
+        raise ValueError('User must specify variable(s) to plot:\n%s' % '\n\t'.join(ifiles[0].variables.keys()))
+    
+    plot(ifiles, args)
--- scripts/pncmap.py
+++ scripts/pncmap.py
@@ -2,112 +2,134 @@
 # Run this script with pnc options
 import os
 from PseudoNetCDF.pncload import PNCConsole
-from PseudoNetCDF.pncparse import pncparser
-from PseudoNetCDF.coordutil import getmap, getlatbnds, getlonbnds
+from PseudoNetCDF.pncparse import pncparse
+from PseudoNetCDF.coordutil import getmap, getlatbnds, getlonbnds, getybnds, getxbnds
 from warnings import warn
-import numpy as np
-ifiles, options = pncparser(has_ofile = True, plot_options = True)
 
+import numpy as np
 import pylab as pl
 Normalize = pl.matplotlib.colors.Normalize
+
 LogNorm = pl.matplotlib.colors.LogNorm
 BoundaryNorm = pl.matplotlib.colors.BoundaryNorm
 
-ifile = ifiles[0]
-map = getmap(ifile)
-fig = pl.gcf()
-ax = pl.gca()
-if options.coastlines: map.drawcoastlines(ax = ax)
-if options.countries: map.drawcountries(ax = ax)
-if options.states: map.drawstates(ax = ax)
-if options.counties: map.drawcounties(ax = ax)
-nborders = len(ax.collections)
+def prepmap(ifiles, options):
+    ifile = ifiles[0]
+    ax = pl.gca()
+    map = getmap(ifile, resolution = 'c')
+    if options.coastlines: map.drawcoastlines(ax = ax)
+    if options.countries: map.drawcountries(ax = ax)
+    if options.states: map.drawstates(ax = ax)
+    if options.counties: map.drawcounties(ax = ax)
+    options.map = map
 
-for ifile in ifiles:
-    if map.projection == 'cyl':
-        lat = ifile.variables['latitude']
-        lon = ifile.variables['longitude']
-        latb, latunit = getlatbnds(ifile)[:]
-        lonb, lonunit = getlonbnds(ifile)[:]
-    else:
-        lat = ifile.variables['latitude']
-        lon = ifile.variables['longitude']
-        latb, latunit = getybnds(ifile)[:]
-        lonb, lonunit = getxbnds(ifile)[:]
+def makemap(ifile, options):
+    fig = pl.gcf()
+    ax = pl.gca()
+    map = options.map
+    nborders = len(ax.collections)
+    for fi, ifile in enumerate(ifiles):
+        if map.projection == 'lcc':
+            lat = ifile.variables['latitude']
+            lon = ifile.variables['longitude']
+            latb, latunit = getybnds(ifile)[:]
+            lonb, lonunit = getxbnds(ifile)[:]
+        else:
+            lat = ifile.variables['latitude']
+            lon = ifile.variables['longitude']
+            latb, latunit = getlatbnds(ifile)[:]
+            lonb, lonunit = getlonbnds(ifile)[:]
     
-    if latb.ndim == lonb.ndim and lonb.ndim == 2:
-        LON, LAT = lonb, latb
-    else:
-        LON, LAT = np.meshgrid(lonb, latb)
+        if latb.ndim == lonb.ndim and lonb.ndim == 2:
+            LON, LAT = lonb, latb
+        else:
+            LON, LAT = np.meshgrid(lonb, latb)
     
-    variables = options.variables
-    if variables is None:
-        variables = [key for key, var in ifile.variables.iteritems() if len(set(['latitude', 'longitude']).intersection(getattr(var, 'coordinates', '').split())) == 2]
-    if len(variables) == 0:
-        raise ValueError('Unable to heuristically determin plottable variables; use -v to specify variables for plotting')
-    for varkey in variables:
-        ax = pl.gca()
+        variables = options.variables
+        if variables is None:
+            variables = [key for key, var in ifile.variables.iteritems() if len(set(['latitude', 'longitude']).intersection(getattr(var, 'coordinates', '').split())) == 2]
+        if len(variables) == 0:
+            raise ValueError('Unable to heuristically determin plottable variables; use -v to specify variables for plotting')
+        for varkey in variables:
+            ax = pl.gca()
                     
-        if not options.overlay:
-            del ax.collections[nborders:]
-        var = ifile.variables[varkey]
-        vals = var[:].squeeze()
-        if options.normalize is None:
-            from scipy.stats import normaltest
-            vmin, vmax = vals.min(), vals.max()
-            if normaltest(vals.ravel())[1] < 0.05:
-                boundaries = np.logspace(np.log10(vmin), np.log10(vmax), num = 10)
-                warn('Selected LogScale for %s' % varkey)
+            if not options.overlay:
+                del ax.collections[nborders:]
+            var = ifile.variables[varkey]
+            vals = var[:].squeeze()
+            if options.normalize is None:
+                from scipy.stats import normaltest
+                vmin, vmax = vals.min(), vals.max()
+                if normaltest(vals.ravel())[1] < 0.05:
+                    boundaries = np.logspace(np.log10(vmin), np.log10(vmax), num = 10)
+                    warn('Selected LogScale for %s' % varkey)
+                else:
+                    boundaries = np.linspace(vmin, vmax, num = 10)
+                norm = BoundaryNorm(boundaries, ncolors = 256)
             else:
-                boundaries = np.linspace(vmin, vmax, num = 10)
-            norm = BoundaryNorm(boundaries, ncolors = 256)
-        else:
-            norm = eval(options.normalize)
-        varunit = getattr(var, 'units', 'unknown').strip()
-        print varkey,
-        if vals.ndim == 1:
-            patches = map.scatter(lon[:], lat[:], c = vals, s = 24, norm = norm, ax = ax)
-        else:
-            patches = map.pcolor(LON, LAT, vals, norm = norm, ax = ax)
-        if lonunit == 'x (LCC m)':
-            ax.xaxis.get_major_formatter().set_scientific(True)
-            ax.xaxis.get_major_formatter().set_powerlimits((-3, 3))
-        if latunit == 'y (LCC m)':
-            ax.yaxis.get_major_formatter().set_scientific(True)
-            ax.yaxis.get_major_formatter().set_powerlimits((-3, 3))
-        ax.set_xlabel(lonunit)
-        ax.set_ylabel(latunit)
-        height = LAT.max() - LAT.min()
-        width = LON.max() - LON.min()
-        if width > height:
-            orientation = 'horizontal'
-        else:
-            orientation = 'vertical'
-        try:
-            cax = cbar.ax
-        except:
-            cax = None
-        cbar = pl.gcf().colorbar(patches, orientation = orientation, cax = cax)
-        del cbar.ax.texts[:]
-        cbar.set_label(varkey + ' (' + varunit + ')')
-        if orientation == 'vertical':
-            cbar.ax.text(.5, 1, '%.2g' % var[:].max(), horizontalalignment = 'center', verticalalignment = 'bottom')
-            cbar.ax.text(.5, 0, '%.2g ' % var[:].min(), horizontalalignment = 'center', verticalalignment = 'top')
-        else:
-            cbar.ax.text(1, .5, ' %.2g' % var[:].max(), verticalalignment = 'center', horizontalalignment = 'left')
-            cbar.ax.text(0, .5, '%.2g ' % var[:].min(), verticalalignment = 'center', horizontalalignment = 'right')
-        try:
-            cbar.formatter.set_scientific(True)
-            cbar.formatter.set_powerlimits((-3, 3))
-        except:
-            pass
-        cbar.update_ticks()
-        fmt = 'png'
-        figpath = os.path.join(options.outpath + '_map_' + varkey + '.' + fmt)
-        if options.interactive:
-            csl = PNCConsole(locals = globals())
-            csl.interact()
+                norm = eval(options.normalize)
+            vmin, vmax = norm.vmin, norm.vmax
+            varunit = getattr(var, 'units', 'unknown').strip()
+            print varkey,
+            if vals.ndim == 1:
+                patches = map.scatter(lon[:], lat[:], c = vals, s = 24, norm = norm, ax = ax)
+            else:
+                patches = map.pcolor(LON, LAT, vals, norm = norm, ax = ax)
+            if lonunit == 'x (LCC m)':
+                ax.xaxis.get_major_formatter().set_scientific(True)
+                ax.xaxis.get_major_formatter().set_powerlimits((-3, 3))
+            if latunit == 'y (LCC m)':
+                ax.yaxis.get_major_formatter().set_scientific(True)
+                ax.yaxis.get_major_formatter().set_powerlimits((-3, 3))
+            ax.set_xlabel(lonunit)
+            ax.set_ylabel(latunit)
+            height = LAT.max() - LAT.min()
+            width = LON.max() - LON.min()
+            if width > height:
+                orientation = 'horizontal'
+            else:
+                orientation = 'vertical'
+            try:
+                cax = cbar.ax
+            except:
+                cax = None
+            if vals.max() > vmax and vals.min() < vmin:
+                extend = 'both'
+            elif vals.max() > vmax:
+                extend = 'max'
+            elif vals.min() < vmin:
+                extend = 'min'
+            else:
+                extend = 'neither'
+            cbar = pl.gcf().colorbar(patches, orientation = orientation, cax = cax, extend = extend)
+            del cbar.ax.texts[:]
+            cbar.set_label(varkey + ' (' + varunit + ')')
+            if orientation == 'vertical':
+                cbar.ax.text(.5, 1.05, '%.3g' % var[:].max(), horizontalalignment = 'center', verticalalignment = 'bottom')
+                cbar.ax.text(.5, -.06, '%.3g ' % var[:].min(), horizontalalignment = 'center', verticalalignment = 'top')
+            else:
+                cbar.ax.text(1.05, .5, ' %.3g' % var[:].max(), verticalalignment = 'center', horizontalalignment = 'left')
+                cbar.ax.text(-.06, .5, '%.3g ' % var[:].min(), verticalalignment = 'center', horizontalalignment = 'right')
+            try:
+                cbar.formatter.set_scientific(True)
+                cbar.formatter.set_powerlimits((-3, 3))
+            except:
+                pass
+            cbar.update_ticks()
+            fmt = 'png'
+            outpath = options.outpath
+            if len(ifiles) > 1:
+                
+                outpath += ('_%%0%dd' % len(str(len(ifiles)))) % fi
+            figpath = os.path.join(outpath + '_map_' + varkey + '.' + fmt)
+            if options.interactive:
+                csl = PNCConsole(locals = globals())
+                csl.interact()
             
-        pl.savefig(figpath)
-        print 'Saved fig', figpath
+            pl.savefig(figpath)
+            print 'Saved fig', figpath
         
+if __name__ == '__main__':
+    ifiles, options = pncparse(has_ofile = True, plot_options = True, interactive = True)
+    prepmap(ifiles, options)
+    makemap(ifiles, options)
