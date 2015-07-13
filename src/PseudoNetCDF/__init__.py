322189709741b4a30b6b826bb1f15383f42b13ab
Barron Henderson <barronh@gmail.com>
2015-07-13 13:21:46 -0400

    Adding pretty warnings.

--- src/PseudoNetCDF/__init__.py
+++ src/PseudoNetCDF/__init__.py
@@ -16,6 +16,15 @@ __all__ = ['sci_var',
            'geoschemfiles',
            'icarttfiles',
            'toms', 'getfile', 'conventions']
+import sys
+import os
+import warnings
+warn = warnings.warn
+def clean_showwarning(message, category, filename, lineno, file=None):
+    print >> sys.stderr, '**PNC:%s:%s:%s:\n  %s' % (os.path.basename(filename), lineno, category.__name__, message)
+    return
+warnings.showwarning = clean_showwarning
+
 import sci_var
 from sci_var import *
 __all__ += sci_var.__all__
