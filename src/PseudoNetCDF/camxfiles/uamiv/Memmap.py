f82038181e9272513db984a262b64bc6974d149c
Barron Henderson <barronh@gmail.com>
2016-03-31 10:28:53 -0400

    Adding a little-endian option
    It is now possible to specify little-endian for CAMx. This is basically
    for when someone accidentally incorrectly compiles.

--- src/PseudoNetCDF/camxfiles/uamiv/Memmap.py
+++ src/PseudoNetCDF/camxfiles/uamiv/Memmap.py
@@ -23,20 +23,16 @@ import struct
 from warnings import warn
 
 #Site-Packages
-from numpy import zeros,array,where,memmap,newaxis,dtype,nan,linspace
+from numpy import zeros, array, where, memmap, newaxis, dtype, nan, linspace
 
 #This Package modules
-from PseudoNetCDF.camxfiles.timetuple import timediff,timeadd
+from PseudoNetCDF.camxfiles.timetuple import timediff, timeadd
 from PseudoNetCDF.camxfiles.FortranFileUtil import OpenRecordFile
 from PseudoNetCDF.sci_var import PseudoNetCDFFile, PseudoIOAPIVariable, PseudoNetCDFVariables
 from PseudoNetCDF.ArrayTransforms import ConvertCAMxTime
 from PseudoNetCDF.camxfiles.units import get_uamiv_units
 from PseudoNetCDF.conventions.ioapi import add_cf_from_ioapi
 #for use in identifying uncaught nan
-listnan=struct.unpack('>f',b'\xff\xc0\x00\x00')[0]
-checkarray=zeros((1,),'f')
-checkarray[0]=listnan
-array_nan=checkarray[0]
 class uamiv(PseudoNetCDFFile):
     """
     uamiv provides a PseudoNetCDF interface for CAMx
@@ -65,22 +61,24 @@ class uamiv(PseudoNetCDFFile):
     """
     
 
-    __emiss_hdr_fmt=dtype(dict(names=['SPAD','name','note','itzon','nspec','ibdate','btime','iedate','etime','EPAD'],formats=['>i','(10,4)>S1','(60,4)>S1','>i','>i','>i','>f','>i','>f','>i']))
-    __grid_hdr_fmt=dtype(dict(names=['SPAD','plon','plat','iutm','xorg','yorg','delx','dely','nx','ny','nz','iproj','istag','tlat1','tlat2','rdum','EPAD'],formats=['>i','>f','>f','>i','>f','>f','>f','>f','>i','>i','>i','>i','>i','>f','>f','>f','>i']))
-    __cell_hdr_fmt=dtype(dict(names=['SPAD','ione1','ione2','nx','ny','EPAD'],formats=['>i','>i','>i','>i','>i','>i']))
-    __time_hdr_fmt=dtype(dict(names=['SPAD','ibdate','btime','iedate','etime','EPAD'],formats=['>i','>i','>f','>i','>f','>i']))
-    __spc_fmt=dtype("(10,4)>S1")
+
     
     __ione=1
     __idum=0
     __rdum=0.
-    def __init__(self,rf,mode='r', P_ALP = None, P_BET = None, P_GAM = None, XCENT = None, YCENT = None, GDTYP = None):
+    def __init__(self, rf, mode='r', P_ALP = None, P_BET = None, P_GAM = None, XCENT = None, YCENT = None, GDTYP = None, endian = 'big'):
         """
         Initialization included reading the header and learning
         about the format.
         
         see __readheader and __gettimestep() for more info
         """
+        ep = self.__endianprefix = dict(big = '>', little = '<')[endian]
+        self.__emiss_hdr_fmt=dtype(dict(names=['SPAD', 'name', 'note', 'itzon', 'nspec', 'ibdate', 'btime', 'iedate', 'etime', 'EPAD'], formats=[ep + 'i', '(10, 4)%sS1' % ep, '(60,4)%sS1' % ep, ep + 'i', ep + 'i', ep + 'i', ep + 'f', ep + 'i', ep + 'f', ep + 'i']))
+        self.__grid_hdr_fmt=dtype(dict(names=['SPAD', 'plon', 'plat', 'iutm', 'xorg', 'yorg', 'delx', 'dely', 'nx', 'ny', 'nz', 'iproj', 'istag', 'tlat1', 'tlat2', 'rdum', 'EPAD'], formats=[ep + 'i', ep + 'f', ep + 'f', ep + 'i', ep + 'f', ep + 'f', ep + 'f', ep + 'f', ep + 'i', ep + 'i', ep + 'i', ep + 'i', ep + 'i', ep + 'f', ep + 'f', ep + 'f', ep + 'i']))
+        self.__cell_hdr_fmt=dtype(dict(names=['SPAD', 'ione1', 'ione2', 'nx', 'ny', 'EPAD'], formats=[ep + 'i', ep + 'i', ep + 'i', ep + 'i', ep + 'i', ep + 'i']))
+        self.__time_hdr_fmt=dtype(dict(names=['SPAD', 'ibdate', 'btime', 'iedate', 'etime', 'EPAD'], formats=[ep + 'i', ep + 'i', ep + 'f', ep + 'i', ep + 'f', ep + 'i']))
+        self.__spc_fmt=dtype("(10,4)%sS1" % ep)
         self.__rffile=rf
         self.__mode=mode
         
@@ -94,9 +92,9 @@ class uamiv(PseudoNetCDFFile):
         ncols=self.NCOLS=len(self.dimensions['COL'])
         nvars=self.NVARS=len(self.dimensions['VAR'])
         nsteps=self.NSTEPS=len(self.dimensions['TSTEP'])
-        setattr(self,'VAR-LIST',"".join([i.ljust(16) for i in self.__var_names__]))
-        self.NAME=self.__emiss_hdr['name'][0,:,0].copy().view('S10')[0].decode()
-        self.NOTE=self.__emiss_hdr['note'][0,:,0].copy().view('S60')[0].decode()
+        setattr(self, 'VAR-LIST', "".join([i.ljust(16) for i in self.__var_names__]))
+        self.NAME=self.__emiss_hdr['name'][0, :, 0].copy().view('S10')[0].decode()
+        self.NOTE=self.__emiss_hdr['note'][0, :, 0].copy().view('S60')[0].decode()
         self.ITZON=self.__emiss_hdr['itzon'][0]
         self.FTYPE = 1 ;
         self.VGTYP = 2 ;
@@ -106,13 +104,13 @@ class uamiv(PseudoNetCDFFile):
         self.UPNAM = "CAMx            " ;
         self.FILEDESC = "CAMx            ";
         # Create variables
-        self.variables=PseudoNetCDFVariables(self.__variables,self.__var_names__+['TFLAG','ETFLAG'])
+        self.variables=PseudoNetCDFVariables(self.__variables, self.__var_names__+['TFLAG', 'ETFLAG'])
         tflag = ConvertCAMxTime(self.__memmap__['DATE']['BDATE'], self.__memmap__['DATE']['BTIME'], self.NVARS)
-        etflag = ConvertCAMxTime(self.__memmap__['DATE']['EDATE'], self.__memmap__['DATE']['ETIME'],self.NVARS)
+        etflag = ConvertCAMxTime(self.__memmap__['DATE']['EDATE'], self.__memmap__['DATE']['ETIME'], self.NVARS)
         tflagv = self.createVariable('TFLAG', 'i', ('TSTEP', 'VAR', 'DATE-TIME'), values = tflag, units = 'DATE-TIME', long_name = 'TFLAG'.ljust(16), var_desc = 'TFLAG'.ljust(80))
         etflagv = self.createVariable('ETFLAG', 'i', ('TSTEP', 'VAR', 'DATE-TIME'), values = etflag, units = 'DATE-TIME', long_name = 'ETFLAG'.ljust(16), var_desc = 'Ending TFLAG'.ljust(80))
         
-        self.SDATE,self.STIME=self.variables['TFLAG'][0,0,:]
+        self.SDATE, self.STIME=self.variables['TFLAG'][0,0,:]
         self.TSTEP =  self.variables['ETFLAG'][0,0,1] - self.variables['TFLAG'][0,0,1]
         if not P_ALP is None:
             self.P_ALP = P_ALP
@@ -135,8 +133,8 @@ class uamiv(PseudoNetCDFFile):
 
         
     def __checkfilelen(self):
-        f=open(self.__rffile,'rb')
-        f.seek(0,2)
+        f=open(self.__rffile, 'rb')
+        f.seek(0, 2)
         flen=f.tell()
         f.close()
         return flen
@@ -154,18 +152,19 @@ class uamiv(PseudoNetCDFFile):
         if not grid_hdr['SPAD'] == grid_hdr['EPAD'] and grid_hdr['SPAD'] == grid_hdr.dtype.itemsize - 8:
             return False
         offset += grid_hdr.dtype.itemsize * grid_hdr.size
-        cell_hdr = memmap(path, mode = 'r', dtype = self.__cell_hdr_fmt, shape=1,offset=offset)
+        cell_hdr = memmap(path, mode = 'r', dtype = self.__cell_hdr_fmt, shape=1, offset=offset)
         if not cell_hdr['SPAD'] == cell_hdr['EPAD'] and cell_hdr['SPAD'] == cell_hdr.dtype.itemsize - 8:
             return False
         return True
         
     def __readheader(self):
+        ep = self.__endianprefix
         offset=0
-        self.__emiss_hdr=memmap(self.__rffile,mode=self.__mode,dtype=self.__emiss_hdr_fmt,shape=1,offset=offset)
+        self.__emiss_hdr=memmap(self.__rffile, mode=self.__mode, dtype=self.__emiss_hdr_fmt, shape=1, offset=offset)
         nspec=self.__emiss_hdr['nspec'][0]
         offset+=self.__emiss_hdr.dtype.itemsize*self.__emiss_hdr.size
 
-        self.__grid_hdr=memmap(self.__rffile,mode=self.__mode,dtype=self.__grid_hdr_fmt,shape=1,offset=offset)
+        self.__grid_hdr=memmap(self.__rffile, mode=self.__mode, dtype=self.__grid_hdr_fmt, shape=1, offset=offset)
 
         self.XORIG=self.__grid_hdr['xorg'][0]
         self.YORIG=self.__grid_hdr['yorg'][0]
@@ -204,23 +203,23 @@ class uamiv(PseudoNetCDFFile):
         
         nx=self.__grid_hdr['nx'][0]
         ny=self.__grid_hdr['ny'][0]
-        nz=max(self.__grid_hdr['nz'],array([1]))[0]
+        nz=max(self.__grid_hdr['nz'], array([1]))[0]
 
         offset+=self.__grid_hdr.dtype.itemsize*self.__grid_hdr.size
-        self.__cell_hdr=memmap(self.__rffile,mode=self.__mode,dtype=self.__cell_hdr_fmt,shape=1,offset=offset)
+        self.__cell_hdr=memmap(self.__rffile, mode=self.__mode, dtype=self.__cell_hdr_fmt, shape=1, offset=offset)
         
         
         offset+=self.__cell_hdr.dtype.itemsize*self.__cell_hdr.size+4
-        self.__spc_hdr=memmap(self.__rffile,mode=self.__mode,dtype=self.__spc_fmt,shape=nspec,offset=offset)
+        self.__spc_hdr=memmap(self.__rffile, mode=self.__mode, dtype=self.__spc_fmt, shape=nspec, offset=offset)
         
         offset+=self.__spc_hdr.dtype.itemsize*self.__spc_hdr.size+4
         
         
-        date_time_fmt=dtype(dict(names=['SPAD','BDATE','BTIME','EDATE','ETIME','EPAD'],formats=['>i','>i','>f','>i','>f','>i']))
+        date_time_fmt=dtype(dict(names=['SPAD', 'BDATE', 'BTIME', 'EDATE', 'ETIME', 'EPAD'], formats=[ep + 'i', ep + 'i', ep + 'f', ep + 'i', ep + 'f', ep + 'i']))
         date_time_block_size=6
-        spc_1_lay_fmt=dtype(dict(names=['SPAD','IONE','SPC','DATA','EPAD'],formats=['>i','>i','(10,4)>S1','(%d,%d)>f' % (ny,nx),'>i']))
+        spc_1_lay_fmt=dtype(dict(names=['SPAD', 'IONE', 'SPC', 'DATA', 'EPAD'], formats=[ep + 'i', ep + 'i', '(10,4)%sS1' % ep, '(%d,%d)%sf' % (ny, nx, ep), ep + 'i']))
         spc_1_lay_block_size=13+nx*ny
-        spc_3d_fmt=dtype((spc_1_lay_fmt,(nz,)))
+        spc_3d_fmt=dtype((spc_1_lay_fmt, (nz,)))
         spc_3d_block_size=spc_1_lay_block_size*nz
         
         # Get species names from spc_hdr
@@ -228,11 +227,11 @@ class uamiv(PseudoNetCDFFile):
         var_names = [v.decode() if hasattr(v, 'decode') else v for v in var_names]
         self.__var_names__ = [''.join(v).strip() for v in var_names]
 
-        data_block_fmt=dtype(dict(names=['DATE']+self.__var_names__,formats=[date_time_fmt]+[spc_3d_fmt]*nspec))
+        data_block_fmt=dtype(dict(names=['DATE']+self.__var_names__, formats=[date_time_fmt]+[spc_3d_fmt]*nspec))
         
         data_block_size=date_time_block_size+nspec*nz*spc_1_lay_block_size
         f=open(self.__rffile)
-        f.seek(0,2)
+        f.seek(0, 2)
         size=f.tell()
         f.close()
         del f
@@ -241,22 +240,22 @@ class uamiv(PseudoNetCDFFile):
             raise ValueError("Not an even number of times %f" % ntimes)
         ntimes=int(ntimes)
         
-        self.createDimension('LAY',nz)
-        self.createDimension('COL',nx)
-        self.createDimension('ROW',ny)
-        tstep = self.createDimension('TSTEP',ntimes)
+        self.createDimension('LAY', nz)
+        self.createDimension('COL', nx)
+        self.createDimension('ROW', ny)
+        tstep = self.createDimension('TSTEP', ntimes)
         tstep.setunlimited(True)
-        self.createDimension('VAR',nspec)
+        self.createDimension('VAR', nspec)
         
-        self.__memmap__=memmap(self.__rffile,mode=self.__mode,dtype=data_block_fmt,offset=offset)
+        self.__memmap__=memmap(self.__rffile, mode=self.__mode, dtype=data_block_fmt, offset=offset)
 
-    def __variables(self,k):
+    def __variables(self, k):
         spc_index=self.__var_names__.index(k)
-        dimensions=('TSTEP','LAY','ROW','COL')
+        dimensions=('TSTEP', 'LAY', 'ROW', 'COL')
         outvals=self.__memmap__[k]['DATA']
         units = get_uamiv_units(self.NAME, k)
         
-        return PseudoIOAPIVariable(self,k,'f',dimensions,values=outvals, units = units)
+        return PseudoIOAPIVariable(self, k, 'f', dimensions, values=outvals, units = units)
 
     def sync(self):
         pass
@@ -276,7 +275,7 @@ class TestMemmap(unittest.TestCase):
         emissfile=uamiv(PseudoNetCDF.testcase.camxfiles_paths['uamiv'])
         emissfile.variables['TFLAG']
         v=emissfile.variables['NO2']
-        self.assert_((v==array([ 0.00000000e+00, 0.00000000e+00, 0.00000000e+00, 0.00000000e+00, 0.00000000e+00, 0.00000000e+00, 1.24175494e-04, 2.79196858e-04, 1.01672206e-03, 4.36782313e-04, 0.00000000e+00, 1.54810550e-04, 3.90250643e-04, 6.18023798e-04, 3.36963218e-04, 0.00000000e+00, 1.85579920e-04, 1.96825975e-04, 2.16468165e-04, 2.19882189e-04],dtype='f').reshape(1, 1, 4, 5)).all())
+        self.assert_((v==array([ 0.00000000e+00, 0.00000000e+00, 0.00000000e+00, 0.00000000e+00, 0.00000000e+00, 0.00000000e+00, 1.24175494e-04, 2.79196858e-04, 1.01672206e-03, 4.36782313e-04, 0.00000000e+00, 1.54810550e-04, 3.90250643e-04, 6.18023798e-04, 3.36963218e-04, 0.00000000e+00, 1.85579920e-04, 1.96825975e-04, 2.16468165e-04, 2.19882189e-04], dtype='f').reshape(1, 1, 4, 5)).all())
 
 if __name__ == '__main__':
     unittest.main()
