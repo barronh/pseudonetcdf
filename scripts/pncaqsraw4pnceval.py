60cedc645bb8c92b0d6e5958f18770e6b160d5fa
Barron Henderson <barronh@gmail.com>
2016-06-28 01:27:46 -0400

    Add daily/hourly options
    script can now read hourly or daily files.

--- scripts/pncaqsraw4pnceval.py
+++ scripts/pncaqsraw4pnceval.py
@@ -32,6 +32,8 @@ Example Workflow:
     $ pncgen -s LAY,0 --extract="-87.881412,30.498001/-85.802182,33.281261/..." CCTM_V5g_par_Linux2_x86_64gfort.ACONC.CMAQ-BENCHMARK_20060801 Benchmark_20060801-20060801.nc
     $ pnceval AQS_DATA_20060801-20060801.nc Benchmark_20060801-20060801.nc
     """)
+parser.add_argument('--sampleval', default = None, help = 'Defaults to "Sample Measurement" for hourly and "Arithmetic Mean" for daily')
+parser.add_argument('--timeresolution', choices = ['daily', 'hourly'], default = 'hourly', help = 'Defaults to hourly')
 parser.add_argument('-s', '--start-date', required = True, dest = 'bdate', type = getbdate, help = 'Start date (inclusive) YYYY-MM-DD')
 parser.add_argument('-e', '--end-date', required = True, dest = 'edate', type = getedate, help = 'Start date (inclusive) YYYY-MM-DD')
 parser.add_argument('-r', '--ref-date', default = datetime(1900, 1, 1), dest = 'rdate', type = getrdate, help = 'Reference date YYYYMMDD HH:MM:SS')
@@ -43,6 +45,13 @@ parser.add_argument('-o', '--output', type = str, dest = 'outpath', nargs = '?',
 parser.add_argument('-O', '--overwrite', dest = 'overwrite', default = False, action = 'store_true', help = 'Ovewrite if output already exists.')
 
 args = parser.parse_args()
+if args.sampleval is None:
+    if args.timeresolution == 'hourly':
+        args.sampleval = "Sample Measurement"
+    elif args.timeresolution == 'daily':
+        args.sampleval = "Arithmetic Mean"
+    else:
+        raise KeyError()
 
 if args.outpath is None:
     args.outpath = 'AQS_DATA_%s-%s.nc' % (args.bdate.strftime('%Y%m%d'), args.edate.strftime('%Y%m%d'))
@@ -67,8 +76,11 @@ if not args.GRIDCRO2D is None:
 bounds = loads(args.wktpolygon)
 pbounds = prep(bounds)
         
-ntimes = int((args.edate - args.bdate).total_seconds() / 3600) + 1
-alltimes = [timedelta(hours = i) + args.bdate for i in range(ntimes)]
+
+nseconds = {'hourly': 3600, 'daily': 3600*24}[args.timeresolution]
+tunit = {'hourly': 'hours', 'daily': 'days'}[args.timeresolution]
+ntimes = int((args.edate - args.bdate).total_seconds() / nseconds) + 1
+alltimes = [timedelta(**{tunit: i}) + args.bdate for i in range(ntimes)]
 
 
 # http://aqsdr1.epa.gov/aqsweb/aqstmp/airdata/download_files.html#Raw
@@ -79,24 +91,49 @@ def hourly_parser(*args):
     out =  (args[0] + ' ' + args[1].astype('S2')+'Z').astype(np.datetime64)
     return out
 
+def hourly_parser(*args):
+    import dateutil
+    parse = np.vectorize(dateutil.parser.parse)
+    out =  (args[0] + ' ' + args[1].astype('S2')+'Z').astype(np.datetime64)
+    return out
+
 years = np.arange(args.bdate.year, args.edate.year + 1)
 hourlys = []
 for year in years:
-    yearpath = 'hourly_%s_%s.csv' % (args.param, year)
+    yearpath = '%s_%s_%s.csv' % (args.timeresolution, args.param, year)
+    filepath = '%s_%s_%s.zip' % (args.timeresolution, args.param, year)
     print('Downloading', yearpath)
+    url = 'http://aqsdr1.epa.gov/aqsweb/aqstmp/airdata/%s' % (filepath,)
     if not os.path.exists(yearpath):
-        getcmd = 'wget --continue http://aqsdr1.epa.gov/aqsweb/aqstmp/airdata/hourly_%s_%s.zip' % (args.param, year)
-        os.system(getcmd)
-        os.system('unzip hourly_%s_%s.zip' % (args.param, year))
+        if not os.path.exists(filepath):
+            from urllib.request import urlopen, Request
+            req = Request(url)
+            ret = urlopen(req)
+            data = ret.read()
+            zipout = open(filepath, 'wb')
+            zipout.write(data)
+            zipout.flush()
+            zipout.close()
+        
+        import zipfile
+        zf = zipfile.ZipFile(filepath)
+        zf.extract(yearpath)        
+
     print('Reading', yearpath)
-    data = pandas.read_csv(yearpath, index_col = False, converters = {'State Code': str, 'County Code': str, 'Site Num': str}, parse_dates = [[11, 12]])
+    if args.timeresolution == 'hourly':
+        parse_dates = [[11, 12]]
+        date_key = 'Date GMT_Time GMT'
+    else:
+        parse_dates = [11]
+        date_key = 'Date Local'
+    data = pandas.read_csv(yearpath, index_col = False, converters = {'State Code': str, 'County Code': str, 'Site Num': str}, parse_dates = parse_dates)
     inspace = np.array([pbounds.contains(Point(plon, plat)) for plon, plat in zip(data['Longitude'].values, data['Latitude'].values)])
-    intime = (data['Date GMT_Time GMT'] >= args.bdate) & \
-       (data['Date GMT_Time GMT'] < args.edate)
+    intime = (data[date_key] >= args.bdate) & \
+       (data[date_key] < args.edate)
     mask = inspace & intime
        
 
-    hourly = data[mask].groupby(['Parameter Code', 'Parameter Name', 'Units of Measure', 'Date GMT_Time GMT', 'State Code', 'County Code', 'Site Num'], as_index = False).aggregate(np.mean).sort(['Parameter Code', 'Parameter Name', 'Units of Measure', 'Date GMT_Time GMT', 'State Code', 'County Code', 'Site Num'])
+    hourly = data[mask].groupby(['Parameter Code', 'Parameter Name', 'Units of Measure', date_key, 'State Code', 'County Code', 'Site Num'], as_index = False).aggregate(np.mean).sort_values(by = ['Parameter Code', 'Parameter Name', 'Units of Measure', date_key, 'State Code', 'County Code', 'Site Num'])
     
     hourlys.append(hourly)
 
@@ -120,8 +157,8 @@ outf.SITENAMES = ';'.join(sitelist)
 
 outf.createDimension('points', len(sitelist))
 outf.lonlatcoords = '/'.join(['%f,%f' % (row['Longitude'], row['Latitude']) for idx, row in sites.iterrows()])
-last_time = start_time = hourly['Date GMT_Time GMT'].values.min()
-end_time = hourly['Date GMT_Time GMT'].values.max()
+last_time = start_time = hourly[date_key].values.min()
+end_time = hourly[date_key].values.max()
 temp = {}
 lat = outf.createVariable('latitude', 'f', ('points',))
 lat.units = 'degrees_north'
@@ -135,8 +172,8 @@ lon[:] = sites['Longitude'].values
 print('Processing data rows')
 
 for idx, row in hourly.iterrows():
-    this_time = row['Date GMT_Time GMT']
-    val = row['Sample Measurement']
+    this_time = row[date_key]
+    val = row[args.sampleval]
     unit = row['Units of Measure'].strip()
     aqsid = row['State Code'] + row['County Code'] + row['Site Num']
     sidx = sitelist.index(aqsid)
@@ -164,8 +201,8 @@ for varkey, tempvals in temp.items():
     outf.variables[varkey][:] = tempvals
 
 time = outf.createVariable('time', 'f', ('time',))
-time.units = args.rdate.strftime('hours since %F')
+time.units = args.rdate.strftime(tunit + ' since %F')
 time.standard_name = 'time'
-time[:] = [(t - args.rdate).total_seconds() / 3600 for t in alltimes]
+time[:] = [(t - args.rdate).total_seconds() / nseconds for t in alltimes]
 outf.sync()
 print('Successful')
