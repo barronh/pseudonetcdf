a626638353bf3baca0be28403562c283bf1ed0b4
Barron Henderson <barronh@gmail.com>
2016-12-01 09:19:36 -0500

    updating make2ds style

--- scripts/pnc2d.py
+++ scripts/pnc2d.py
@@ -8,7 +8,7 @@ def main():
     #parser.add_argument('--no-squeeze', dest = 'squeeze', default = True, action = 'store_false', help = 'Squeeze automatically removes singleton dimensions; disabling requires user to remove singleton dimensions with --remove-singleton option')
     parser.add_argument('--swapaxes', action = 'store_true', help = 'Swap x-y axes')
     ifiles, args = pncparse(has_ofile = True, plot_options = True, interactive = True, parser = parser)
-    make2ds(ifiles, args)
+    make2ds(args)
         
 if __name__ == '__main__':
     main()
