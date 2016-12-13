f541f4eaed428e0b5ad46bfcbeefd3b1e07bb049
barronh <barronh@gmail.com>
2016-12-13 16:48:56 -0500

    Adding basic documentation to userfuncs.py

--- src/PseudoNetCDF/userfuncs.py
+++ src/PseudoNetCDF/userfuncs.py
@@ -4,8 +4,12 @@ def mda8(arr, axis = None, keepdims = True):
     Daily-Maximum 8-hour average concentration
        - can be applied to any dimensions, but make sense with time
        - returns the day max of the a8
-    arguments:
-       arr
+    Arguments:
+       arr - array like
+       axis - axis over which to apply mda8
+       keepdims - should be true
+    Returns:
+       out - maximum of 8 element running average. For masked inptus, only values with 6 or more valid entries are returned
     """
     if hasattr(arr, 'mask'):
         arr_m = np.ma.masked_array(arr)
@@ -18,21 +22,45 @@ def mda8(arr, axis = None, keepdims = True):
     return arrmda8
 
 def _dayfunc(func, arr, axis = None, keepdims = True):
+    """
+    Arguments:
+       func - function with reduceat property
+       arr - array_like
+       axis - axis overwhich to apply func.reduceat
+       keepdims - must be true
+    Returns:
+       out - array_like with func.reduceat applied at 24 element increments
+    """
     daystartend = np.arange(0, arr.shape[axis], 24)
-    return func.reduceat(arr, daystartend, axis = axis)[:-1]
+    return func.reduceat(arr, daystartend, axis = axis)[(slice(None),)*axis + (slice(-1),)]
 
 def daymax(arr, axis = None, keepdims = True):
+    """
+    see _dayfunc with np.maximum as func
+    """
     return _dayfunc(np.maximum, arr, axis = axis, keepdims = keepdims)
 
 def daymin(arr, axis = None, keepdims = True):
+    """
+    see _dayfunc with np.minimum as func
+    """
     return _dayfunc(np.minimum, arr, axis = axis, keepdims = keepdims)
 
 def daymean(arr, axis = None, keepdims = True):
+    """
+    see _dayfunc with np.mean as func
+    """
     return _dayfunc(np.mean, arr, axis = axis, keepdims = keepdims)
 
 def daystd(arr, axis = None, keepdims = True):
+    """
+    see _dayfunc with np.std as func
+    """
     return _dayfunc(np.std, arr, axis = axis, keepdims = keepdims)
 
 def dayvar(arr, axis = None, keepdims = True):
+    """
+    see _dayfunc with np.var as func
+    """
     return _dayfunc(np.var, arr, axis = axis, keepdims = keepdims)
 
