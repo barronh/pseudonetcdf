98513da44247c459365701cda9c771c3c48a7d81
barronh <barronh@gmail.com>
2016-12-30 16:24:58 -0500

    Fixing vertprofile

--- scripts/pncvertprofile.py
+++ scripts/pncvertprofile.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
-from PseudoNetCDF.plotutil.vertprofile import vertprofileplot, vertparser
-from PseudoNetCDF.pncparse import pncparse
+from PseudoNetCDF.plotutil.vertprofile import vertprofileplot, add_vertprofile_options
+from PseudoNetCDF.pncparse import pncparse, getparser
 
 if __name__ == '__main__':
+    vertparser = getparser(has_ofile = True, plot_options = True, interactive = False)
+    add_vertprofile_options(vertparser)
     ifiles, args = pncparse(has_ofile = True, parser = vertparser)
     vertprofileplot(ifiles, args)
