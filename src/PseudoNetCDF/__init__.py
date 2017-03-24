6964ef6c69ecb687f4c6f68ef1a59b1ba500cf17
barronh <barronh@gmail.com>
2017-03-24 15:21:03 -0400

    Fixing warning system for Python v3.6
    The call signature should have always included the optional keyword argument line, but previously worked without it.

--- src/PseudoNetCDF/__init__.py
+++ src/PseudoNetCDF/__init__.py
@@ -21,7 +21,7 @@ import sys
 import os
 import warnings
 warn = warnings.warn
-def clean_showwarning(message, category, filename, lineno, file=None):
+def clean_showwarning(message, category, filename, lineno, file = None, line = None):
     print('**PNC:%s:%s:%s:\n  %s' % ((filename), lineno, category.__name__, message), file = sys.stderr)
     return
 warnings.showwarning = clean_showwarning
@@ -46,3 +46,4 @@ from . import coordutil
 from . import test
 from ._getreader import anyfile
 from .pncparse import PNC
+
