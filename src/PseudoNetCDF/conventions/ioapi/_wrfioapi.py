3b075c00ba35ce0b58830089e46bc14c8df6bc1d
barronh <barronh@gmail.com>
2017-05-25 11:34:53 -0400

    Enabling wrfioapi

--- src/PseudoNetCDF/conventions/ioapi/_wrfioapi.py
+++ src/PseudoNetCDF/conventions/ioapi/_wrfioapi.py
@@ -1,7 +1,7 @@
 import numpy as np
 _coorddict = dict(west_east = 'longitude', south_north = 'latitude', Time = 'time', bottom_top = 'altitude',
                   west_east_stag = 'longitude', south_north_stag = 'latitude', Time_stag = 'time', bottom_top_stag = 'altitude',)
-def add_cf_from_wrfioapi(ifile):
+def add_cf_from_wrfioapi(ifile, coordkeys = []):
     try:
         for invark, outvark in [('XLONG', 'longitude'), ('XLAT', 'latitude')]:
             try:
@@ -13,11 +13,11 @@ def add_cf_from_wrfioapi(ifile):
             for pk in invar.ncattrs():
                 setattr(outvar, pk, getattr(invar, pk))
             outvar[:] = invar[0]
-    except: pass
-            
-    for k in ifile.variables.keys():
-        var = ifile.variables[k]
-        var.coordinates = ' '.join([_coorddict.get(dk, dk) for dk in var.dimensions])
+    except Exception as e:
+        print(e)
+        pass
+    
+    # Add time coordinate if not available.
     try:
         outvar = ifile.createVariable('time', 'i', ('Time',))
         invar = ifile.variables['Times']
@@ -29,20 +29,29 @@ def add_cf_from_wrfioapi(ifile):
         timesince = (np.array([np.datetime64(time[0].replace('_', 'T') + 'Z') for time in invals]) - sdate).astype('l')
         outvar[:] = timesince
     except: pass
-    for varkey in ifile.variables.keys():
-        var = ifile.variables[varkey]
-        try:
-            ifile.variables[varkey] = var
-        except:
-            pass
-        olddims = list(var.dimensions)
-        dims = map(lambda x: {'west_east': 'latitude', 'north_south': 'longitude', 'Time': 'time', 'LAY': 'level'}.get(x, x), olddims)
-        dims = [d for d in dims] # Why was I excluding time  if d != 'time'
-        if olddims != dims:
-            if ('PERIM' in dims or 
-                ('latitude' in dims and 'longitude' in dims)
-               ) and varkey not in ('latitude', 'longitude'):
-                var.coordinates = ' '.join(dims)
+    # add projection
+    grid_mapping_name = get_proj(ifile)
+    # Add x and y coordinates
+    try:
+        gridvar = ifile.variables[grid_mapping_name]
+        xvar = ifile.createVariable('x', 'f', ('west_east',))
+        xvar.units = 'm'
+        xvar.standard_name = 'x'
+        xvar[:] = np.arange(xvar.size)*ifile.DX-gridvar.false_easting  + ifile.DX/2
+        yvar = ifile.createVariable('y', 'f', ('south_north',))
+        yvar.units = 'm'
+        yvar.standard_name = 'y'
+        yvar[:] = np.arange(yvar.size)*ifile.DY-gridvar.false_northing  + ifile.DY/2
+    except Exception as e:
+        raise e
+    for k in ifile.variables.keys():
+        var = ifile.variables[k]
+        olddims = [dk for dk in var.dimensions]
+        newdims = [_coorddict.get(dk, dk) for dk in var.dimensions]
+        if olddims != newdims:
+            var.coordinates = ' '.join(newdims)
+            var.grid_mapping = grid_mapping_name
+    
 
     ifile.Conventions = 'CF-1.6'
     
@@ -59,34 +68,34 @@ def get_proj(ifile):
     DX, DY - required for MAP_PROJ = 1, 2, 3 (defaults to 0 otherwise)
     LATINC, LONINC - required for MAP_PROJ = 6 (defaults to 0 otherwise)
     """
-    projname = {1: "lcc", 2: 'npstere', 3: 'merc'}
     
-    if projname == 'merc':
-        var = ifile.createVariable('mercator', 'i', ())
-        var.grid_mapping_name = 'mercator'
+    projname = {1: "lambert_conformal_conic", 2: 'polar_stereographic', 3: 'mercator'}[ifile.MAP_PROJ]
+    var = ifile.createVariable(projname, 'i', ())
+    var.grid_mapping_name = projname
+    var.earth_radius = 6370000.
+    var.false_easting = 0
+    var.false_northing = 0
+    x0_from_cen = len(ifile.dimensions['west_east']) / 2 * ifile.DX
+    y0_from_cen = len(ifile.dimensions['south_north']) / 2 * ifile.DY
+    if projname == 'mercator':
         var.longitude_of_projection_origin = ifile.STAND_LON
         var.standard_parallel = ifile.TRUELAT1
-        var.false_easting = len(ifile.dimensions['west_east']) / 2
-        var.false_northing = len(ifile.dimensions['south_north']) / 2
-    elif projname == "lcc":
-        var = ifile.createVariable('mercator', 'i', ())
-        var.grid_mapping_name = 'lambert_conformal_conic'
-        var.standard_parallel = np.array([ifile.TRUELAT1, ifile.TRUELAT1])
+    elif projname == "lambert_conformal_conic":
+        var.standard_parallel = np.array([ifile.TRUELAT1, ifile.TRUELAT2])
         var.longitude_of_central_meridian = ifile.STAND_LON
-        var.latitude_of_projection_origin = ifile.REF_LAT
-        var.false_easting = 0
-        var.false_northing = 0
-
-    elif projname == 'npstere':
-        if ifile.TRUELAT1 > 0:
-            projname = 'npstere'
-        else:
-            projname = 'spstere'
-        var = ifile.createVariable('mercator', 'i', ())
-        var.grid_mapping_name = 'polar_stereographic'
+        var.latitude_of_projection_origin = ifile.MOAD_CEN_LAT
+    elif projname == 'polar_stereographic':
         var.straight_vertical_longitude_from_pole = ifile.STAND_LON
         var.latitude_of_projection_origin = ifile.TRUELAT1
-        var.standard_parallel = ifile.REF_LALT
-        var.false_easting = 0
-        var.false_northing = 0
-
+        var.standard_parallel = ifile.MOAD_CEN_LAT
+    
+    from PseudoNetCDF.coordutil import getproj4_from_cf_var
+    from mpl_toolkits.basemap import pyproj
+    projstr = getproj4_from_cf_var(var)
+    proj = pyproj.Proj(projstr)
+    gcx, gcy = proj(ifile.CEN_LON, ifile.CEN_LAT)
+    glx = gcx - x0_from_cen
+    gly = gcy - y0_from_cen
+    var.false_easting = -glx
+    var.false_northing = -gly
+    return projname
