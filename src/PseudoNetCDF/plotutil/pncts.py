84c808852b52e44cb0d19a0d25af5b75ac56efdb
barronh <barronh@gmail.com>
2018-01-12 11:46:34 -0500

    improving timeseries
    Switching secondary and primary axis and adding plotcommands functionality

--- src/PseudoNetCDF/plotutil/pncts.py
+++ src/PseudoNetCDF/plotutil/pncts.py
@@ -9,14 +9,14 @@ def plotts(args):
     ifiles = args.ifiles
     times = [gettimes(ifile) for ifile in ifiles]
     fig = plt.figure()
-    sax = fig.add_subplot(111)#add_axes([.1, .15, .8, .8])
-    sax.set_xlabel('Time (UTC)')
+    ax = fig.add_subplot(111)#add_axes([.1, .15, .8, .8])
+    ax.set_xlabel('Time (UTC)')
     split = 25
     for target in args.variables:
         vars = [ifile.variables[target] for ifile in ifiles]
         unit = getattr(vars[0], 'units', 'unknown')
-        sax.set_ylabel(target + '(' + unit + ')')
-        del sax.lines[:]
+        ax.set_ylabel(target + '(' + unit + ')')
+        del ax.lines[:]
         nvars = len(vars)
         varwidth = .8/nvars/1.1
         po = np.arange(24) + 0.1 + varwidth/2
@@ -25,10 +25,12 @@ def plotts(args):
             if args.squeeze:
                 vals = vals.squeeze()
             vardesc = getattr(var, 'description', None)
-            varb = sax.plot(time, vals[:], label = vardesc)
-        #plt.setp(sax.xaxis.get_ticklabels(),rotation = 45)
+            varb = ax.plot(time, vals[:], label = vardesc)
+        #plt.setp(ax.xaxis.get_ticklabels(),rotation = 45)
         plt.legend()
         figpath = args.outpath + target + '.' + args.figformat
+        for pc in args.plotcommands:
+           exec(pc, globals(), locals())
         fig.savefig(figpath)
         if args.verbose > 0: print('Saved fig', figpath)
         print(figpath)
