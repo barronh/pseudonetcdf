81cab311fb9a7f32465c8393476591d5c48fa233
barronh <barronh@gmail.com>
2018-01-12 11:41:55 -0500

    Help and Meta-Data improvements
    Still experimental

--- src/PseudoNetCDF/cmaqfiles/_ioapi.py
+++ src/PseudoNetCDF/cmaqfiles/_ioapi.py
@@ -40,6 +40,10 @@ class ioapi_base(PseudoNetCDFFile):
         PseudoNetCDFFile.setncatts(self, attdict)
     
     def subsetVariables(self, varkeys, inplace = False):
+        """
+        Calls PseudoNetCDF.subsetVariables and updates VAR-LIST,
+        VAR, and TFLAG
+        """
         if not 'TFLAG' in varkeys:
             varkeys = ['TFLAG'] + list(varkeys)
         varkeylen = 16
@@ -49,11 +53,16 @@ class ioapi_base(PseudoNetCDFFile):
         sliceo = [vi for vi, varkey in enumerate(varlist) if varkey in varkeys]
         newvarlist = [varkey for varkey in varlist if varkey in varkeys]
         outf = outf.sliceDimensions(VAR = sliceo)
-        outf.NVAR = len(outf.dimensions['VAR'])
+        outf.NVARS = len(outf.dimensions['VAR'])
         setattr(outf, 'VAR-LIST', ''.join([vk.ljust(16) for vk in newvarlist]))
         return outf
     
     def sliceDimensions(self, *args, **kwds):
+        """
+        Calls PseudoNetCDF.sliceDimensions and
+        corrects ROW, COL, LAY and TIME meta-data
+        according to the ioapi format
+        """
         import numpy as np
         outf = PseudoNetCDFFile.sliceDimensions(self, *args, **kwds)
         dimslices = kwds.copy()
@@ -68,16 +77,14 @@ class ioapi_base(PseudoNetCDFFile):
             if isarray['ROW'] and isarray['COL']:
                deleterowcol = True
         if 'LAY' in kwds:
-            outf.VGLVLS[kwds['LAY']]
+            outf = outf.VGLVLS[kwds['LAY']]
         if deleterowcol:
             del outf.dimensions['COL']
             del outf.dimensions['ROW']
         else:
             if 'COL' in kwds and 'COL' in outf.dimensions:
-                outf.NCOLS = len(outf.dimensions['COL'])
                 outf.XORIG += np.r_[kwds['COL']][0] * outf.XCELL
             if 'ROW' in kwds and 'COL' in outf.dimensions:
-                outf.NROWS = len(outf.dimensions['ROW'])
                 outf.YORIG += np.r_[kwds['ROW']][0] * outf.YCELL
         if 'TSTEP' in kwds:
             import datetime
@@ -89,12 +96,15 @@ class ioapi_base(PseudoNetCDFFile):
                 if not (dt[0] == dt).all():
                     warn('New time is unstructured')
                 outf.TSTEP = int((datetime.datetime(1900, 1, 1, 0) + dt[0]).strftime('%H%M%S'))
+        
+        outf.updatemeta()
         return outf
     
-    def interpSigma(self, vglvls, interptype = 'linear'):
+    def interpSigma(self, vglvls, newvgtop = None, interptype = 'linear'):
         """
         self - the file to interpolate from must have VGLVLS
         vglvls - the new vglvls (edges)
+        newvgtop - Converting to new
         interptype - 'linear' or 'conserve'
              linear - uses a linear interpolation
              conserve - uses a mass conserving interpolation
@@ -102,8 +112,13 @@ class ioapi_base(PseudoNetCDFFile):
         import numpy as np
         from scipy.interpolate import interp1d
         # input sigma coordinates
-        zs = (self.VGLVLS[:-1]+self.VGLVLS[1:])/2
-        nzs = (vglvls[:-1]+vglvls[1:])/2
+        myvglvls = self.VGLVLS
+        if not newvgtop is None:
+            dp0 = 101325. - self.VGTOP
+            dp1 = 101325. - newvgtop
+            myvglvls = (myvglvls * dp0 + self.VGTOP - newvgtop) / dp1
+        zs = (myvglvls[:-1]+myvglvls[1:])/2.
+        nzs = (vglvls[:-1]+vglvls[1:])/2.
         if interptype == 'linear':
             # output sigma coordinates
             # identity matrix
@@ -116,15 +131,39 @@ class ioapi_base(PseudoNetCDFFile):
                 newdata = (weights * data[:, None]).sum(0)
                 return newdata
         elif interptype == 'conserve':
-            coeff = _sigma2coeff(self.VGLVLS, vglvls) # (Nold, Nnew)
-            dp_in = -np.diff(self.VGLVLS.astype('d'))[:, None]
+            coeff = _sigma2coeff(myvglvls, vglvls) # (Nold, Nnew)
+            dp_in = -np.diff(myvglvls.astype('d'))[:, None]
             #dp_out = -np.diff(VGLVLS.astype('d'))[:, None]
             fdp = dp_in * coeff
             ndp = fdp.sum(0)
             def interpsigma(data):
                 nvals = (data[:, None] * fdp).sum(0) / ndp
                 return nvals
+        
         outf = self.applyAlongDimensions(LAY = interpsigma)
+        outf.VGLVLS = vglvls
+        outf.updatemeta()
+        return outf
+    
+    def updatemeta(self):
+        import numpy as np
+        import datetime
+        if 'LAY' in self.dimensions: self.NLAYS = len(self.dimensions['LAY'])
+        if 'COL' in self.dimensions: self.NCOLS = len(self.dimensions['COL'])
+        if 'ROW' in self.dimensions: self.NCOLS = len(self.dimensions['ROW'])
+        if 'VAR' in self.dimensions: self.NVARS = len(self.dimensions['VAR'])
+        times = self.getTimes()
+        self.SDATE = int(times[0].strftime('%Y%j'))
+        self.STIME = int(times[0].strftime('%H%M%S'))
+        if times.size > 1:
+            dt = np.diff(times)
+            if not (dt[0] == dt).all():
+                warn('New time is unstructured')
+            self.TSTEP = int((datetime.datetime(1900, 1, 1, 0) + dt[0]).strftime('%H%M%S'))
+        
+    def applyAlongDimensions(self, *args, **kwds):
+        outf = PseudoNetCDFFile.applyAlongDimensions(self, *args, **kwds)
+        outf.updatemeta()
         return outf
 
 class ioapi(Dataset, ioapi_base):
