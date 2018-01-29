1a7bc093dd09b33f7aac4195963c87e603fbd5a4
Barron Henderson <barronh@gmail.com>
2018-01-28 20:39:52 -0500

    Adding documentation and functionality

--- src/PseudoNetCDF/cmaqfiles/_ioapi.py
+++ src/PseudoNetCDF/cmaqfiles/_ioapi.py
@@ -1,13 +1,39 @@
 from PseudoNetCDF.pncwarn import warn
 from netCDF4 import Dataset
 from ..core._files import PseudoNetCDFFile
+from collections import OrderedDict
+import numpy as np
+import datetime
+today = datetime.datetime.today()
+# Assuming some fo the most common
+# options
+_i = _ioapi_defaults = OrderedDict()
+_i['IOAPI_VERSION'] = "N/A".ljust(80)
+_i['EXEC_ID'] = "????????????????".ljust(80)
+_i['FTYPE'] = -1
+_i['CDATE'] = int(today.strftime('%Y%j'))
+_i['CTIME'] = int(today.strftime('%H%M%S'))
+_i['WDATE'] = int(today.strftime('%Y%j'))
+_i['WTIME'] = int(today.strftime('%H%M%S'))
+_i['NTHIK'] = 1
+_i['GDTYP'] = 1
+_i['P_ALP'] = 33.
+_i['P_BET'] = 45.
+_i['P_GAM'] = -97.
+_i['XCENT'] = -97.
+_i['YCENT'] = 40.
+_i['VGTYP'] = 2
+_i['VGTOP'] = np.float32(5000)
+_i['GDNAM'] = "UNKNOWN         "
+_i['UPNAM'] = "MAKEIOAPI       "
+_i['FILEDESC'] = "".ljust(80)
+_i['HISTORY'] = ""
 
 def _sigma2coeff(fromvglvls, tovglvls):
     """
     Calculate fraction of pressure from each layer in fromfile
     that is in each layer in tofile and return matrix
     """
-    import numpy as np
     edges = np.interp(tovglvls[::-1], fromvglvls[::-1], np.arange(fromvglvls.size)[::-1])[::-1].repeat(2,0)[1:-1].reshape(-1, 2).astype('d')
     coeff = np.zeros((fromvglvls.size - 1, tovglvls.size - 1), dtype = 'd')
     for li, (b, t) in enumerate(edges):
@@ -26,67 +52,131 @@ def _sigma2coeff(fromvglvls, tovglvls):
 class ioapi_base(PseudoNetCDFFile):
     def __getattributte__(self, *args, **kwds):
         return getattr(self, *args, **kwds)
+    
     @classmethod
     def isMine(self, path):
         return False
     
-    def _newlike(self):
-        if isinstance(self, PseudoNetCDFFile):
-            outt = ioapi_base
-            outf = outt.__new__(outt)
-        else:
-            outf = PseudoNetCDFFile()
-        return outf 
+    def _updatetime(self, write = True, create = False):
+        from datetime import datetime
+        t = datetime.now()
+        try:
+            if create:
+                self.CDATE = int(t.strftime('%Y%j'))
+                self.CTIME = int(t.strftime('%H%M%S'))
+            if write:
+                self.WDATE = int(t.strftime('%Y%j'))
+                self.WTIME = int(t.strftime('%H%M%S'))
+        except Exception as e:
+            warn('Time could not be updated; ' + str(e))
+        
     def setncatts(self, attdict):
+        """
+        Wrapper on PseudoNetCDF.setncatts that updates WDATE, and WTIME
+        
+        See also
+        --------
+        see PseudoNetCDFFile.setncatts
+        """
         PseudoNetCDFFile.setncatts(self, attdict)
+        self._updatetime()
+
+    def createVariable(self, name, type, dimensions, fill_value = None, **properties):
+        """
+        Wrapper on PseudoNetCDF.createVariable that updates VAR-LIST,
+        NVARS, VAR, and TFLAG
+        
+        See also
+        --------
+        see PseudoNetCDFFile.createVariable
+        """
+        out = PseudoNetCDFFile.createVariable(self, name = name, type = type, dimensions = dimensions, fill_value = fill_value, **properties)
+        self._add2Varlist([name])
+        return out
     
-    def subsetVariables(self, varkeys, inplace = False):
+    def copyVariable(self, var, key = None, dtype = None, dimensions = None, fill_value = None, withdata = True):
         """
-        Calls PseudoNetCDF.subsetVariables and updates VAR-LIST,
-        VAR, and TFLAG
+        Wrapper on PseudoNetCDF.copyVariable that updates VAR-LIST,
+        NVARS, VAR, and TFLAG
+        
+        See also
+        --------
+        see PseudoNetCDFFile.copyVariable
         """
-        if not 'TFLAG' in varkeys:
-            varkeys = ['TFLAG'] + list(varkeys)
-        varkeylen = 16
-        varliststr = getattr(self, 'VAR-LIST')
-        varlist = list(varliststr[0+i:varkeylen+i].strip() for i in range(0, len(varliststr), varkeylen))
-        outf = PseudoNetCDFFile.subsetVariables(self, varkeys, inplace = inplace)
-        sliceo = [vi for vi, varkey in enumerate(varlist) if varkey in varkeys]
-        newvarlist = [varkey for varkey in varlist if varkey in varkeys]
+        outvar = PseudoNetCDFFile.copyVariable(self, var, key = key, dtype = dtype, dimensions = dimensions, fill_value = fill_value, withdata = withdata)
+        if key is None:
+            for propk in ['name', 'standard_name', 'long_name']:
+                if hasattr(var, propk):
+                    key = getattr(var, propk)
+            else:
+                raise AttributeError('varkey must be supplied because var has no name, standard_name or long_name')
+        self._add2Varlist([key])
+        return outvar
+    
+    def subsetVariables(self, varkeys, inplace = False, exclude = False):
+        """
+        Wrapper on PseudoNetCDFFile.subsetVariables that updates VAR-LIST,
+        NVARS, VAR, and TFLAG
+        
+        See also
+        --------
+        see PseudoNetCDFFile.sliceDimensions
+        """
+        varlist = self.getVarlist(update = False)
+        newvarlist = [varkey for varkey in varlist if (varkey in varkeys) != exclude]
+        outf = PseudoNetCDFFile.subsetVariables(self, varkeys, inplace = inplace, exclude = exclude)
+        if not 'TFLAG' in outf.variables and 'TFLAG' in self.variables:
+            outf.copyVariable(self.variables['TFLAG'], key = 'TFLAG')
+        sliceo = np.array([vi for vi, varkey in enumerate(varlist) if varkey in newvarlist])
         outf = outf.sliceDimensions(VAR = sliceo)
-        outf.NVARS = len(outf.dimensions['VAR'])
-        setattr(outf, 'VAR-LIST', ''.join([vk.ljust(16) for vk in newvarlist]))
+        setattr(outf, 'VAR-LIST', '')
+        outf._add2Varlist(newvarlist)
         return outf
     
     def sliceDimensions(self, *args, **kwds):
         """
-        Calls PseudoNetCDF.sliceDimensions and
-        corrects ROW, COL, LAY and TIME meta-data
-        according to the ioapi format
+        Wrapper PseudoNetCDFFile.sliceDimensions that corrects ROW, COL,
+        LAY and TIME meta-data according to the ioapi format
+        
+        Parameters
+        ----------
+        see PseudoNetCDFFile.sliceDimensions
         """
-        import numpy as np
+        # First slice as normal
         outf = PseudoNetCDFFile.sliceDimensions(self, *args, **kwds)
+        # Copy slice keywords excluding newdims
         dimslices = kwds.copy()
         dimslices.pop('newdims', None)
         
+        # Identify array indices and the need for fancy indexing
         isarray = {dk: not np.isscalar(dv) for dk, dv in dimslices.items()}
         anyisarray = np.sum(list(isarray.values())) > 1
+        
+        # Check if COL or ROW was used
         hascol = 'COL' in dimslices
         hasrow = 'ROW' in dimslices
         deleterowcol = False
         if hascol and hasrow:
             if isarray['ROW'] and isarray['COL']:
                deleterowcol = True
+        
+        # If lay was subset, subset VGLVLS too
         if 'LAY' in kwds:
             outf = outf.VGLVLS[kwds['LAY']]
+        
+        # If subsetting replaces ('ROW', 'COL') ... for example with ('PERIM',)
+        # remove the dimensions
         if deleterowcol:
             del outf.dimensions['COL']
             del outf.dimensions['ROW']
         else:
+            # Update origins
             if 'COL' in kwds and 'COL' in outf.dimensions:
                 outf.XORIG += np.r_[kwds['COL']][0] * outf.XCELL
             if 'ROW' in kwds and 'COL' in outf.dimensions:
                 outf.YORIG += np.r_[kwds['ROW']][0] * outf.YCELL
+        
+        # Update TFLAG, SDATE, STIME and TSTEP
         if 'TSTEP' in kwds:
             import datetime
             times = np.atleast_1d(self.getTimes()[kwds['TSTEP']])
@@ -101,66 +191,203 @@ class ioapi_base(PseudoNetCDFFile):
         outf.updatemeta()
         return outf
     
-    def interpSigma(self, vglvls, newvgtop = None, interptype = 'linear'):
+    def interpSigma(self, vglvls, vgtop = None, interptype = 'linear', extrapolate = False, fill_value = 'extrapolate'):
         """
-        self - the file to interpolate from must have VGLVLS
-        vglvls - the new vglvls (edges)
-        newvgtop - Converting to new
-        interptype - 'linear' or 'conserve'
-             linear - uses a linear interpolation
-             conserve - uses a mass conserving interpolation
+        Parameters
+        ----------
+        self : the file to interpolate from must have VGLVLS
+        vglvls : the new vglvls (edges)
+        vgtop : Converting to new vgtop
+        interptype : 'linear' or 'conserve'
+             linear : uses a linear interpolation
+             conserve : uses a mass conserving interpolation
+        extrapolate : allow extrapolation beyond bounds with linear, default False
+        fill_value : set fill value (e.g, nan) to prevent extrapolation or edge 
+                     continuation
+        
+        Returns
+        -------
+        outf - ioapi_base PseudoNetCDFFile with al variables interpolated
+        
+        Notes
+        -----
+        When extrapolate is false, the edge values are used for points beyond
+        the inputs.
         """
-        import numpy as np
-        from scipy.interpolate import interp1d
-        # input sigma coordinates
+        # grab input sigma coordinates
         myvglvls = self.VGLVLS
-        if not newvgtop is None:
+        
+        # If needed, recalculate this files SIGMA
+        if not vgtop is None and not vgtop == self.VGTOP:
             dp0 = 101325. - self.VGTOP
-            dp1 = 101325. - newvgtop
-            myvglvls = (myvglvls * dp0 + self.VGTOP - newvgtop) / dp1
+            dp1 = 101325. - vgtop
+            myvglvls = (myvglvls * dp0 + self.VGTOP - vgtop) / dp1
+        
+        # Use midpoint for sigmas in inputs and outputs
         zs = (myvglvls[:-1]+myvglvls[1:])/2.
         nzs = (vglvls[:-1]+vglvls[1:])/2.
+        
+        
         if interptype == 'linear':
-            # output sigma coordinates
+            from scipy.interpolate import interp1d
             # identity matrix
             ident = np.identity(zs.size)
-            # weight function
-            weight_func = interp1d(zs, ident, 'linear')
-            # weights
+            # weight function; use bounds outside
+            weight_func = interp1d(zs, ident, kind = 'linear', bounds_error = False, fill_value = 'extrapolate')
+            # calculate weights, which can be reused
             weights = weight_func(nzs)
+            
+            # If not extrapolating, force weights to
+            # no more than one at the edges
+            if not extrapolate:
+                weights = np.maximum(0, weights)
+                weights /= weights.sum(0)
+            
+            # Create a function for interpolation
             def interpsigma(data):
-                newdata = (weights * data[:, None]).sum(0)
+                if data.ndim == 1:
+                    newdata = (weights * data[:, None]).sum(0)
+                else:
+                    newdata = (weights[None, :, :, None, None] * data[:, :, None]).sum(1)
                 return newdata
+            
         elif interptype == 'conserve':
+            # Calculate a weighting matrix using mass conserving
+            # methods
             coeff = _sigma2coeff(myvglvls, vglvls) # (Nold, Nnew)
+            # Calculate input mass fractions
             dp_in = -np.diff(myvglvls.astype('d'))[:, None]
-            #dp_out = -np.diff(VGLVLS.astype('d'))[:, None]
+            
+            # Create a weighted mass fraction function
             fdp = dp_in * coeff
+            
+            # Create a precalculated normalizer
             ndp = fdp.sum(0)
+            
+            # Create a function for interpolation
             def interpsigma(data):
                 nvals = (data[:, None] * fdp).sum(0) / ndp
                 return nvals
         
+        # Apply function on LAY
         outf = self.applyAlongDimensions(LAY = interpsigma)
-        outf.VGLVLS = vglvls
+        
+        # Ensure vglvls is a simple array
+        outf.VGLVLS = vglvls.view(np.ndarray).astype('f')
+        outf.NLAYS = len(outf.VGLVLS) - 1
         outf.updatemeta()
         return outf
     
-    def updatemeta(self):
-        import numpy as np
-        import datetime
+    def _add2Varlist(self, varkeys):
+        varliststr = getattr(self, 'VAR-LIST', '')
+        keys = varliststr.split()
+        newkeys = set(varkeys).difference(keys + ['TFLAG'])
+        for varkey in varkeys:
+            if varkey in newkeys:
+                varliststr += varkey.ljust(16)
+                keys.append(varkey)
+        setattr(self, 'NVARS', len(keys))
+        setattr(self, 'VAR-LIST', varliststr)
+        self._updatetime()
+        return keys
+    
+    def getVarlist(self, update = True):
+        """
+        Returns
+        -------
+        varlist : VAR-LIST split and stripped
+        update  : update files attributes to be consistent
+        
+        Notes
+        -----
+        If VAR-LIST does not exist, it is added assuming all variables
+        with dimensions ('TSTEP', 'LAY', ...) are variables
+        """
+        if not hasattr(self, 'VAR-LIST'):
+            varliststr_old = ''
+            varlist = ''.join([k.ljust(16) for k, v in self.variables.items() if v.dimensions[:2] == ('TSTEP', 'LAY')])
+        else:
+            varliststr_old = getattr(self, 'VAR-LIST').split()
+            varlist = [vk for vk in varliststr_old if vk in self.variables]
+        varliststr_new = ''.join([vk.ljust(16) for vk in varlist])
+        if update and varliststr_new != varliststr_old:
+            setattr(self, 'VAR-LIST', varliststr_new)
+        if update and len(varlist) != self.NVARS:
+            self.NVARS = len(varlist)
+            if 'VAR' in self.dimensions:
+                if self.NVARS != len(self.dimensions['VAR']):
+                    try: self.createDimension('VAR', self.NVARS)
+                    except: pass
+            else:
+                self.createDimension('VAR', self.NVARS)
+
+        return varlist
+        
+    def updatemeta(self, **attdict):
+        """
+        Parameters
+        ----------
+        attdict : key value pairs to update meta data
+        
+        Returns
+        -------
+        None
+        
+        Notes
+        -----
+        Meta data not provided or present will be inferred or made up. (See _ioapi_defaults)
+        """
+        attdict.update(_ioapi_defaults)
+        for pk, pv in attdict.items():
+            if not hasattr(self, pk):
+                setattr(self, pk, pv)
+        
+        if 'TSTEP' in self.dimensions:
+            td = self.dimensions['TSTEP']
+            if not td.isunlimited():
+                td.setunlimited(True)
+        
         if 'LAY' in self.dimensions: self.NLAYS = len(self.dimensions['LAY'])
         if 'COL' in self.dimensions: self.NCOLS = len(self.dimensions['COL'])
         if 'ROW' in self.dimensions: self.NCOLS = len(self.dimensions['ROW'])
-        if 'VAR' in self.dimensions: self.NVARS = len(self.dimensions['VAR'])
-        times = self.getTimes()
-        self.SDATE = int(times[0].strftime('%Y%j'))
-        self.STIME = int(times[0].strftime('%H%M%S'))
+        
+        self.getVarlist()
+        
+        if not 'DATE-TIME' in self.dimensions:
+            self.createDimension('DATE-TIME', 2)
+        
+        self._updatetime()
+        try:
+            times = self.getTimes()
+        except:
+            return
+        
+        if not hasattr(self, 'SDATE'):
+            self.SDATE = int(times[0].strftime('%Y%j'))
+        if not hasattr(self, 'STIME'):
+            self.STIME = int(times[0].strftime('%H%M%S'))
         if times.size > 1:
             dt = np.diff(times)
             if not (dt[0] == dt).all():
                 warn('New time is unstructured')
             self.TSTEP = int((datetime.datetime(1900, 1, 1, 0) + dt[0]).strftime('%H%M%S'))
+        if not 'TFLAG' in self.variables:
+            dotflag = True
+            tvar = self.createVariable('TFLAG', 'i', ('TSTEP', 'VAR', 'DATE-TIME'))
+            tvar.units = '<YYYYDDD, HHMMSS>'.ljust(16)
+            tvar.long_name = 'TFLAG'.ljust(16)
+            tvar.var_desc = "Timestep-valid flags:  (1) YYYYDDD or (2) HHMMSS                                "
+        else:
+            tvar = self.variables['TFLAG']
+            dotflag = ~((self.SDATE == tvar[0,0,0]) and (self.STIME == tvar[0,0,1]))
+            
+        
+        if dotflag:
+            yyyyjjj = np.array([int(t.strftime('%Y%j')) for t in times])
+            hhmmss = np.array([int(t.strftime('%H%M%S')) for t in times])
+            
+            tvar[:, :, 0] = yyyyjjj[:, None]
+            tvar[:, :, 1] = hhmmss[:, None]
         
     def applyAlongDimensions(self, *args, **kwds):
         outf = PseudoNetCDFFile.applyAlongDimensions(self, *args, **kwds)
@@ -171,6 +398,15 @@ class ioapi(Dataset, ioapi_base):
     def __getattribute__(self, *args, **kwds):
         return Dataset.__getattribute__(self, *args, **kwds)
 
+    def _newlike(self):
+        if isinstance(self, PseudoNetCDFFile):
+            outt = ioapi_base
+            outf = outt.__new__(outt)
+        else:
+            outf = PseudoNetCDFFile()
+        outf._updatetime(write = True, create = True)
+        return outf 
+    
     @classmethod
     def isMine(cls, *args, **kwds):
         try:
