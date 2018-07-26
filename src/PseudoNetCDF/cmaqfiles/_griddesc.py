02ea87e70642e6ff4f2d8c8e72fc5242b27b8bea
barronh <barronh@gmail.com>
2018-07-26 10:02:51 -0400

    adding features to griddesc

--- src/PseudoNetCDF/cmaqfiles/_griddesc.py
+++ src/PseudoNetCDF/cmaqfiles/_griddesc.py
@@ -1,61 +1,74 @@
 import PseudoNetCDF as pnc
 from collections import OrderedDict
+import numpy as np
+_prjp = ['GDTYP', 'P_ALP', 'P_BET', 'P_GAM', 'XCENT', 'YCENT']
+_grdp = [
+    'PRJNAME', 'XORIG', 'YORIG', 'XCELL', 'YCELL', 'NCOLS', 'NROWS', 'NTHIK'
+]
 
-"""
+
+class griddesc(pnc.PseudoNetCDFFile):
+    """
+    griddesc is designed to read IOAPI griddesc files and create
+    a file with basic CF metadata. An example format of GRIDDESC
+    is show below with two grids on one projection.
+
 ' '
-'LamCon_40N_97W'
+'LCC'
   2        33.000        45.000       -97.000       -97.000        40.000
 ' '
 'SE52BENCH'
-'LamCon_40N_97W'    792000.000  -1080000.000     12000.000     12000.000 100  80   1
+'LCC'    792000.000  -1080000.000     12000.000     12000.000 100  80   1
 '36US3'
-'LamCon_40N_97W'  -2952000.000  -2772000.000     36000.000     36000.000 172  148   1
+'LCC'  -2952000.000  -2772000.000     36000.000     36000.000 172  148   1
 ' '
 """
-_geop = ['GDTYP', 'P_ALP', 'P_BET', 'P_GAM', 'XCENT', 'YCENT']
-_prjp = ['PRJNAME', 'XORIG', 'YORIG', 'XCELL', 'YCELL', 'NCOLS', 'NROWS', 'NTHIK']
-class griddesc(pnc.PseudoNetCDFFile):
-    def __init__(self, path, GDNAM = None, NLAYS = 1):
+    def __init__(
+        self, path, GDNAM=None, VGLVLS=[1., 0.], VGTOP=5000., FTYPE=1
+    ):
         gdlines = open(path, 'r').read().strip().split('\n')
-        gdlines = [l.strip() for l in gdlines]
+        gdlines = [line.strip() for line in gdlines]
         assert(gdlines[0] == "' '")
         assert(gdlines[-1] == "' '")
         i = 0
-        geo = {}
+        prj = {}
         grd = OrderedDict()
         blanks = []
-        grdnames = []
-        while len(blanks) < 3:
-            l = gdlines[i]
-            if l.strip() == "' '":
+        while i < len(gdlines):
+            line = gdlines[i]
+            if line.strip() == "' '":
                 blanks.append(i)
             else:
                 i += 1
                 parts = [eval(p) for p in gdlines[i].split()]
-                key = eval(l)
+                key = eval(line)
                 if len(blanks) == 1:
-                    geo[key] = dict(zip(_geop, parts))
+                    prj[key] = dict(zip(_prjp, parts))
                 elif len(blanks) == 2:
-                    grd[key] = dict(zip(_prjp, parts))
+                    grd[key] = dict(zip(_grdp, parts))
                 else:
-                   pass
+                    pass
             i += 1
-        self._geo = geo
-        self._grd = grd 
+        self._prj = prj
+        self._grd = grd
+        self.FTYPE = FTYPE
         self.GDNAM = GDNAM
-        self.NLAYS = NLAYS
+        self.VGTOP = VGTOP
+        self.VGLVLS = np.array(VGLVLS, dtype='f')
+        self.NLAYS = len(VGLVLS) - 1
+        self._synthvars = None
         self.addtime()
         self.setgrid()
 
-    def setgrid(self, key = None):
+    def setgrid(self, key=None):
         if key is None:
             if self.GDNAM is None:
-                key = self.grd.keys()[0]
+                key = list(self._grd)[0]
             else:
                 key = self.GDNAM
         grd = self._grd[key]
-        geo = self._geo[grd['PRJNAME']]
-        self.setncatts(geo)
+        prj = self._prj[grd['PRJNAME']]
+        self.setncatts(prj)
         self.setncatts(grd)
         self.adddims()
 
@@ -63,20 +76,43 @@ class griddesc(pnc.PseudoNetCDFFile):
         self.createDimension('TSTEP', 1).setunlimited(True)
         self.createDimension('DATE-TIME', 2)
         self.createDimension('VAR', 1)
-        tflag = self.createVariable('TFLAG', 'i',
+        tflag = self.createVariable(
+            'TFLAG', 'i',
             ('TSTEP', 'VAR', 'DATE-TIME'),
             units='<YYYYJJJ,HHMMSS>',
-            long_name='TFLAG',
-            var_desc='TFLAG'
+            long_name='TFLAG'.ljust(16),
+            var_desc='TFLAG'.ljust(80)
         )
         tflag[:, :, 0] = -635
         tflag[:, :, 1] = 0
 
     def adddims(self):
+        for k in 'LAY ROW COL PERIM'.split():
+            if k in self.dimensions:
+                del self.dimensions[k]
         self.createDimension('LAY', self.NLAYS)
-        self.createDimension('ROW', self.NROWS)
-        self.createDimension('COL', self.NCOLS)
+        if self.FTYPE == 1:
+            self.createDimension('ROW', self.NROWS)
+            self.createDimension('COL', self.NCOLS)
+        elif self.FTYPE == 2:
+            perim = (self.NCOLS * 2 + self.NROWS * 2 + self.NTHIK * 4)
+            self.createDimension('PERIM', perim * self.NTHIK)
+        else:
+            raise ValueError(
+                'Only supports FTYPE 1 or 2; received ' +
+                str(self.FTYPE)
+                )
+
+        if self._synthvars is None:
+            oldkeys = set(self.variables)
+        else:
+            for key in self._synthvars:
+                if key in self.variables:
+                    del self.variables[key]
         pnc.conventions.ioapi.add_cf_from_ioapi(self)
+        if self._synthvars is None:
+            self._synthvars = set(self.variables).difference(oldkeys)
+
 
 if __name__ == '__main__':
     f = griddesc('GRIDDESC', '36US3')
