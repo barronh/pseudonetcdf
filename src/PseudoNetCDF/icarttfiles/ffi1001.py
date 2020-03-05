4442322ff7aae92f99eb84bf34419050ce2c8d99
Christopher Holmes <cdholmes@fsu.edu>
2020-03-04 20:52:51 -0500

    More formatting for Travis CI

--- src/PseudoNetCDF/icarttfiles/ffi1001.py
+++ src/PseudoNetCDF/icarttfiles/ffi1001.py
@@ -273,8 +273,9 @@ Returns:
             llod_val = llod_values[vi]
             ulod_flag = ulod_flags[vi]
             ulod_val = ulod_values[vi]
-            vals = MaskedArray(dat * scale, mask=(dat == miss), fill_value=miss)
-            scale = scales[vi] = 1 # Set to 1 after applying
+            vals = MaskedArray(dat * scale, mask = (dat == miss), 
+                               fill_value=miss)
+            scale = scales[vi] = 1  # Set to 1 after applying
             tmpvar = self.variables[var] = PseudoNetCDFVariable(
                 self, var, 'd', ('POINTS',), values=vals)
             tmpvar.units = unit
