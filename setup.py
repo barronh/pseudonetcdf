fcea51ab041bc762e03b1d635f0ec80b350489d8
barronh <barronh@gmail.com>
2026-02-23 11:56:20 -0500

    add explicit v3 versiosn

--- setup.py
+++ setup.py
@@ -109,6 +109,10 @@ setup(
     url='http://github.com/barronh/pseudonetcdf/',
     classifiers=[
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
         'Operating System :: MacOS',
         'Operating System :: Microsoft :: Windows',
         'Operating System :: POSIX',
